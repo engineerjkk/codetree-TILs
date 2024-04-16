4bfbcd2da6485c2daa79e66acc92fedf32323ab8
JuneKoo Kang <engineerjkk@naver.com>
2024-04-16 20:13:28 +0900

    Title: 배열의 평균, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240416/배열의 평균/ave-of-array.py
@@ -0,0 +1,23 @@
+import sys
+input=sys.stdin.readline
+space=[]
+for i in range(2):
+    space.append(list(map(float,input().split())))
+
+
+for i in (space):
+    print(sum(i)/len(i),end=" ")
+print()
+for j in range(4):
+    a=[]
+    for i in range(2):
+        a.append(space[i][j])
+    b=sum(a)/len(a)
+    print(b,end=" ")
+
+
+total=[]
+for i in range(2):
+    for j in range(4):
+        total.append(space[i][j])
+print(sum(total)/len(total))
