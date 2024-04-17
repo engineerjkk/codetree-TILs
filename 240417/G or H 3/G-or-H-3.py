6fa809aac3e4b5b5addce695fb62e7690933f2c0
JuneKoo Kang <engineerjkk@naver.com>
2024-04-17 14:51:43 +0900

    Title: G or H 3, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240417/G or H 3/G-or-H-3.py
+++ 240417/G or H 3/G-or-H-3.py
@@ -9,7 +9,7 @@ for i in range(n):
     tmp.append(int(lst[i][0]))
 MIN=min(tmp)
 MAX=max(tmp)
-space=[0]*(MAX*MAX+1)
+space=[0]*(MAX*2+1)
 for i,name in lst:
     if name=='G':
         space[int(i)]=1
