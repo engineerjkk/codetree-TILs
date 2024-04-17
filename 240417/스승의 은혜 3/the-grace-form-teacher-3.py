26cf379dc561a9fc0c78d418c79898078d1e14ef
JuneKoo Kang <engineerjkk@naver.com>
2024-04-17 22:37:15 +0900

    Title: 스승의 은혜 3, Time: 5084ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 240417/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240417/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -7,23 +7,17 @@ lst=[]
 for _ in range(n):
     lst.append(list(map(int,input().split())))
 
-
 students=0
-for i in range(2,n+1):
-    tmp=False
+for i in range(1,n+1):
     nCr=combinations(lst,i)
-
     for j in nCr:
         price=[]
         deliver=[]
-        # print("___")
-        # print(j)
         for p,d in j: 
-            price.append(int(p))
-            deliver.append(int(d))
-            #print(len(price))
+            price.append(p)
+            deliver.append(d)
         a=price.index(max(price))
-        price[a]=int(price[a]/2)
+        price[a]=price[a]/2
         total=sum(price)+sum(deliver)
         if b>=total:
             stu=len(price)
