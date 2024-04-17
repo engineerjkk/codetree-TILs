a101b7db7c4d86c80a80bb0c7ffacde0c73a18ed
JuneKoo Kang <engineerjkk@naver.com>
2024-04-17 14:36:12 +0900

    Title: G or H 3, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240417/G or H 3/G-or-H-3.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+n,k = map(int,input().split())
+lst=[]
+for _ in range(n):
+    lst.append(list(map(str,input().split())))
+tmp=[]
+for i in range(len(lst)):
+    tmp.append(int(lst[i][0]))
+MIN=min(tmp)
+MAX=max(tmp)
+space=[0]*(MAX+1)
+for i,name in lst:
+    if name=='G':
+        space[int(i)]=1
+    elif name=='H':
+        space[int(i)]=2
+total=0
+for i in range(MAX-k+1):
+    cnt=0
+    for j in range(i,i+k+1):
+        cnt+=space[j]
+    total=max(total,cnt)
+print(total)
