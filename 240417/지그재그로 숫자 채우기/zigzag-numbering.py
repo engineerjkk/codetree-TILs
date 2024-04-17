2c1106a6be3b3148048ec0428c2f225bd0e6ef4a
JuneKoo Kang <engineerjkk@naver.com>
2024-04-17 13:26:07 +0900

    Title: 지그재그로 숫자 채우기, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240417/지그재그로 숫자 채우기/zigzag-numbering.py
+++ 240417/지그재그로 숫자 채우기/zigzag-numbering.py
@@ -1,9 +1,22 @@
 import sys
 input = sys.stdin.readline
+
 n,m = map(int,input().split())
 
-space=[i for i in range(n*m)]
+space=[[0]*m for _ in range(n)]
 
-for i in range(n):
-    print(space[i],end=" ")
-    print(space[-i-1])
+count=0
+for col in range(m):
+    if col%2==0:
+        for row in range(n):
+            space[row][col]=count
+            count+=1
+    else:
+        for row in range(n-1,-1,-1):
+            space[row][col]=count
+            count+=1
+
+for row in range(n):
+    for col in range(m):
+        print(space[row][col],end=" ")
+    print()
