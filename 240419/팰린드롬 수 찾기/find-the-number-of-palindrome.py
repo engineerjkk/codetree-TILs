2ccb39fb7f7021b4bc88dae3967e1d6ed237cc78
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 00:25:08 +0900

    Title: 팰린드롬 수 찾기, Time: 131ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240419/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 240419/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -6,6 +6,6 @@ for i in range(x,y+1):
     number=list(str(i).strip())
     #print(number)
     for j in range(len(number)//2):
-        if number[j]==number[-j-1]:
+        if number[j]==number[len(number)-j-1]:
             cnt+=1
 print(cnt)
