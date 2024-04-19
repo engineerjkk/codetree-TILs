7d50842ede0f7dd34308020af32de4d8f5f2979b
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 12:40:49 +0900

    Title: 체크판위에서 2, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240419/체크판위에서 2/on-the-checkboard-2.py
+++ 240419/체크판위에서 2/on-the-checkboard-2.py
@@ -4,13 +4,13 @@ input = sys.stdin.readline
 r,c=map(int,input().split())
 space=[]
 for _ in range(r):
-    space.append(list(map(int,input().split())))
+    space.append(list(map(str,input().split())))
 
 cnt=0
 for i in range(r):
     for j in range(c):
         for k in range(i+1,r-1):
             for l in range(j+1,c-1):
-                if space[0][0] != space[i][j] and space[i][j] != space[k][l] and space[k][l] !=space[r][c]:
+                if space[0][0] != space[i][j] and space[i][j] != space[k][l] and space[k][l] !=space[r-1][c-1]:
                     cnt+=1
 print(cnt)
