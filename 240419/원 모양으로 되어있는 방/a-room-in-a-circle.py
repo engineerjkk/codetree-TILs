0b15b8b63b61a8357a6a90fab36667b3218b0e30
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 14:43:19 +0900

    Title: 원 모양으로 되어있는 방, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240419/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -12,12 +12,10 @@ for _ in range(n):
 distance=INT_MAX
 for i in range(n):
     dis=0
-    if i==0:
-        continue
-    else:       
-        for j in range(i):
-            x=queue.popleft()
-            queue.append(x)
+  
+    for j in range(i):
+        x=queue.popleft()
+        queue.append(x)
     for j in range(n):
         dis+=queue[j]*j
     distance=min(distance,dis)
