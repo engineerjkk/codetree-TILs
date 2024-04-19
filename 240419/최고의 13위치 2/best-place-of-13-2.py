31353fb334bf6975019598b0e50e57dd88767228
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 23:48:03 +0900

    Title: 최고의 13위치 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240419/최고의 13위치 2/best-place-of-13-2.py
+++ 240419/최고의 13위치 2/best-place-of-13-2.py
@@ -7,20 +7,20 @@ for _ in range(n):
 value=0
 visited=[[False]*n for _ in range(n)]
 for i in range(n):
-    for j in range(1,n-1):
-        cnt=space[i][j-1]+space[i][j]+space[i][j+1]
+    for j in range(n-2):
+        cnt=space[i][j]+space[i][j+1]+space[i][j+2]
         if cnt>value:
             value=cnt
-            coordinates=[[i,j-1],[i,j],[i,j+1]]
+            coordinates=[[i,j],[i,j+1],[i,j+2]]
 
 for i,j in coordinates:
     visited[i][j]=True
 
 second_value=0
 for i in range(n):
-    for j in range(1,n-1):
-        if not visited[i][j-1] and not visited[i][j] and not visited[i][j+1]:
-            cnt=space[i][j-1]+space[i][j]+space[i][j+1]
+    for j in range(n-2):
+        if not visited[i][j] and not visited[i][j+1] and not visited[i][j+2]:
+            cnt=space[i][j]+space[i][j+1]+space[i][j+2]
             if cnt>second_value:
                 second_value=cnt
 print(value+second_value)
