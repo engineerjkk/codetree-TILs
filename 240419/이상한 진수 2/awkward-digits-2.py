b1e440d8f3aa594e0e43cf34ba2498b25c2ba6ee
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 13:12:16 +0900

    Title: 이상한 진수 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/이상한 진수 2/awkward-digits-2.py
+++ 240419/이상한 진수 2/awkward-digits-2.py
@@ -3,21 +3,26 @@ input = sys.stdin.readline
 arr=list(map(int,input().strip()))
 
 ans=0
-for i in range(len(arr)):
-    ans+=arr[i]*2**(len(arr)-i-1)
-
-for i in range(1,len(arr)):
-    if arr[i]==0:
-        arr[i]=1
+if len(arr)>1:
+    for i in range(len(arr)):
+        ans+=arr[i]*2**(len(arr)-i-1)
+    for i in range(len(arr)):
+        if arr[i]==0:
+            arr[i]=1
+        else:
+            arr[i]=0
+        tmp=0
+        for j in range(len(arr)):
+            tmp+=arr[j]*2**(len(arr)-j-1)
+        ans=max(ans,tmp)
+        if arr[i]==0:
+            arr[i]=1
+        else:
+            arr[i]=0
+else:
+    if arr[0]==1:
+        ans=0
     else:
-        arr[i]=0
-    tmp=0
-    for j in range(len(arr)):
-        tmp+=arr[j]*2**(len(arr)-j-1)
-    ans=max(ans,tmp)
-    if arr[i]==0:
-        arr[i]=1
-    else:
-        arr[i]=0
-    
+        ans=1
+        
 print(ans)
