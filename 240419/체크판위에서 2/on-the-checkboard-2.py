81e7d6f93a1d5ebddd3b9ae0ded970846fc8c3ac
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 12:46:50 +0900

    Title: 체크판위에서 2, Time: 137ms, Memory: 28MB, Status: Passed - CodeTree

--- 240419/체크판위에서 2/on-the-checkboard-2.py
+++ 240419/체크판위에서 2/on-the-checkboard-2.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
-
 r,c=map(int,input().split())
+
 space=[]
 for _ in range(r):
     space.append(list(map(str,input().split())))
@@ -11,6 +11,6 @@ for i in range(1,r):
     for j in range(1,c):
         for k in range(i+1,r-1):
             for l in range(j+1,c-1):
-                if space[0][0] != space[i][j] and space[i][j] != space[k][l] and space[k][l] !=space[-1][-1]:
+                if space[0][0]!=space[i][j] and space[i][j]!=space[k][l] and space[k][l]!=space[-1][-1]:
                     cnt+=1
 print(cnt)
