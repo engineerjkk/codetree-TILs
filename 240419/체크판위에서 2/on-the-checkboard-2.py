7c6973fb1978faf9186216bec27d07b6e448cc41
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 12:41:34 +0900

    Title: 체크판위에서 2, Time: 115ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240419/체크판위에서 2/on-the-checkboard-2.py
+++ 240419/체크판위에서 2/on-the-checkboard-2.py
@@ -11,6 +11,6 @@ for i in range(r):
     for j in range(c):
         for k in range(i+1,r-1):
             for l in range(j+1,c-1):
-                if space[0][0] != space[i][j] and space[i][j] != space[k][l] and space[k][l] !=space[r-1][c-1]:
+                if space[0][0] != space[i][j] and space[i][j] != space[k][l] and space[k][l] !=space[-1][-1]:
                     cnt+=1
 print(cnt)
