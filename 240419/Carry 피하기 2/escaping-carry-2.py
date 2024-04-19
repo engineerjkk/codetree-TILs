a4fe438b6ee8afe777cc020ec4652296e13ffc23
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 19:37:10 +0900

    Title: Carry 피하기 2, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240419/Carry 피하기 2/escaping-carry-2.py
+++ 240419/Carry 피하기 2/escaping-carry-2.py
@@ -7,7 +7,7 @@ for _ in range(n):
 
 ans=0
 for i in range(n-2):
-    arr_i=[0]*5 #0 0 0 0 0
+    arr_i=[0]*4 #0 0 0 0 0
     str_i=str(lst[i]) # 5 2 2
     m=-1
     for l in range(len(str_i)-1,-1,-1):
@@ -15,7 +15,7 @@ for i in range(n-2):
         m-=1
     ##############################
     for j in range(i+1,n-1):
-        arr_j=[0]*5 #0 0 0 0 0
+        arr_j=[0]*4 #0 0 0 0 0
         str_j=str(lst[j]) # 5 2 2
         m=-1
         for l in range(len(str_j)-1,-1,-1):
@@ -23,7 +23,7 @@ for i in range(n-2):
             m-=1
         ##########################
         for k in range(j+1,n):
-            arr_k=[0]*5 #0 0 0 0 0
+            arr_k=[0]*4 #0 0 0 0 0
             str_k=str(lst[k]) # 5 2 2
             m=-1
             for l in range(len(str_k)-1,-1,-1):
@@ -33,7 +33,7 @@ for i in range(n-2):
             for p in range(len(arr_k)):
                 if int(arr_i[p])+int(arr_j[p])+int(arr_k[p])<10:
                     cnt+=1
-            if cnt==5:
+            if cnt==4:
                 a = int(''.join(map(str, arr_i)))
                 b = int(''.join(map(str, arr_j)))
                 c = int(''.join(map(str, arr_k)))
