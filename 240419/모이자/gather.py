eb8b01cd22b7ca858cf2eda404ba6ee79aa227e8
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 10:39:00 +0900

    Title: 모이자, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240419/모이자/gather.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n=int(input())
+lst=list(map(int,input().split()))
+
+dis=100000
+for i in range(n):
+    ans=0
+    for j in range(n):
+        ans+=lst[j]*abs(i-j)
+    dis=min(dis,ans)
+print(dis)
