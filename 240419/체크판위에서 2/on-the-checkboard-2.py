789afcb6912ba96b204b42f11ecd7f14585e2843
JuneKoo Kang <engineerjkk@naver.com>
2024-04-19 12:43:44 +0900

    Title: 체크판위에서 2, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240419/체크판위에서 2/on-the-checkboard-2.py
+++ 240419/체크판위에서 2/on-the-checkboard-2.py
@@ -7,8 +7,8 @@ for _ in range(r):
     space.append(list(map(str,input().split())))
 
 cnt=0
-for i in range(r):
-    for j in range(c):
+for i in range(1,r):
+    for j in range(1,c):
         for k in range(i+1,r-1):
             for l in range(j+1,c-1):
                 if space[0][0] != space[i][j] and space[i][j] != space[k][l] and space[k][l] !=space[-1][-1]:
