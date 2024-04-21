aceeeb7cdf6b422639e5ec88221431bf386445de
JuneKoo Kang <engineerjkk@naver.com>
2024-04-21 22:26:41 +0900

    Title: 바구니 안의 사탕 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240421/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240421/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -4,7 +4,7 @@ n,k = map(int,input().split())
 lst=[]
 for _ in range(n):
     lst.append(list(map(int,input().split())))
-maximum=100000
+maximum=100
 arr=[0]*(maximum+1)
 
 for num,i in lst:
@@ -12,11 +12,10 @@ for num,i in lst:
 
 ans=0
 answer=0
-for i in range(k,maximum-k):
+for i in range(maximum):
     nm=0
     for j in range(i-k,i+k+1):
-        nm+=arr[j]
-    if nm>ans:
-        ans=nm
-        answer=i
+        if -1<j<=maximum:
+            nm+=arr[j]
+    ans=max(ans,nm)
 print(ans)
