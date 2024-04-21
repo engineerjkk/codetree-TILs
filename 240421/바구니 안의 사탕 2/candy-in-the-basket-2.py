9cbd3cbf4808a1c3fe4dcf89cf87d1840a30f660
JuneKoo Kang <engineerjkk@naver.com>
2024-04-21 22:19:45 +0900

    Title: 바구니 안의 사탕 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240421/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+n,k = map(int,input().split())
+lst=[]
+for _ in range(n):
+    lst.append(list(map(int,input().split())))
+arr=[0]*101
+
+for num,i in lst:
+    arr[i]+=num
+
+ans=0
+answer=0
+for i in range(k,101-k):
+    nm=0
+    for j in range(i-k,i+k+1):
+        nm+=arr[j]
+    if nm>ans:
+        ans=nm
+        answer=i
+print(ans)
