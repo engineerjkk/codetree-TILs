fc4a2ca91bf0cd72d73a38e9f665dee964c43dc5
JuneKoo Kang <engineerjkk@naver.com>
2024-04-21 17:21:20 +0900

    Title: 아름다운 수열 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240421/아름다운 수열 2/beautiful-sequence-2.py
+++ 240421/아름다운 수열 2/beautiful-sequence-2.py
@@ -5,14 +5,18 @@ n,m = map(int,input().split())
 n_lst=list(map(int,input().split()))
 m_lst=list(map(int,input().split()))
 
-nCr = permutations(n_lst,m)
+nCr = permutations(m_lst,m)
 cnt=0
 
 for x in nCr:
-    tmp=[]
-    for i in x:
-        tmp.append(i)
-    if tmp == m_lst:
-        cnt+=1
+    tmp2=[]
+    for k in x:
+        tmp2.append(k)
+    for i in range(n-m+1):
+        tmp=[]
+        for j in range(i,i+m):
+            tmp.append(n_lst[j])
+        if tmp==tmp2:
+            cnt+=1
 
 print(cnt)
