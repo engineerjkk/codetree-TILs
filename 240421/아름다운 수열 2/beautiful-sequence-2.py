54fac747b33456ef438cd8606275bade1128dd3c
JuneKoo Kang <engineerjkk@naver.com>
2024-04-21 18:00:50 +0900

    Title: 아름다운 수열 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240421/아름다운 수열 2/beautiful-sequence-2.py
+++ 240421/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,25 +1,12 @@
 import sys
 input = sys.stdin.readline
-from itertools import combinations, combinations_with_replacement,permutations
 n,m = map(int,input().split())
 n_lst=list(map(int,input().split()))
 m_lst=list(map(int,input().split()))
 
-nCr = permutations(m_lst,m)
 cnt=0
-nCr=list(set(nCr))
-test=[]
-for i in nCr:
-    tmp=[]
-    for j in i:
-        tmp.append(str(j))
-    test.append(''.join(tmp))
-tmp=[]
-for i in n_lst:
-    tmp.append(str(i))
-n_str_lst=''.join(tmp)
-for i in range(n-m+1):
-    for x in test:
-        if n_str_lst[i:i+m]==x:
-            cnt+=1
+for i in range(0,n-m+1):
+    if sorted(n_lst[i:i+m])==sorted(m_lst):
+        cnt+=1
+
 print(cnt)
