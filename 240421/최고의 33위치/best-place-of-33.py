9edd64a1f4753ad05aebf11d395c6f540e1a8e0f
JuneKoo Kang <engineerjkk@naver.com>
2024-04-21 23:45:25 +0900

    Title: 최고의 33위치, Time: 177ms, Memory: 25MB, Status: Passed - CodeTree

--- 240421/최고의 33위치/best-place-of-33.py
+++ 240421/최고의 33위치/best-place-of-33.py
@@ -9,8 +9,8 @@ ans=0
 for i in range(n-3+1):
     for j in range(n-3+1):
         nm=0
-        for k in range(i+3):
-            for l in range(j+3):
+        for k in range(i,i+3):
+            for l in range(j,j+3):
                 nm+=space[k][l]
         ans=max(nm,ans)
 print(ans)
