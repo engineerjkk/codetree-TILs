15bb411850d855f425d3600989f7d9203627930e
JuneKoo Kang <engineerjkk@naver.com>
2024-04-21 23:44:21 +0900

    Title: 최고의 33위치, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240421/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+n=int(input())
+space=[]
+for _ in range(n):
+    space.append(list(map(int,input().split())))
+
+ans=0
+for i in range(n-3+1):
+    for j in range(n-3+1):
+        nm=0
+        for k in range(i+3):
+            for l in range(j+3):
+                nm+=space[k][l]
+        ans=max(nm,ans)
+print(ans)
