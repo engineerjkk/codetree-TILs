aa0f23ad3bedd4df912f3219464e07d9fb63ca68
JuneKoo Kang <engineerjkk@naver.com>
2024-04-23 12:48:51 +0900

    Title: 금 채굴하기, Time: 288ms, Memory: 26MB, Status: Passed - CodeTree

--- 240423/금 채굴하기/gold-mining.py
+++ 240423/금 채굴하기/gold-mining.py
@@ -1,27 +1,23 @@
-n,m = tuple(map(int,input().split()))
-grid = [
-    list(map(int,input().split()))
-    for _ in range(n)
-]
+import sys
+input=sys.stdin.readline
+n,m= tuple(map(int,input().split()))
 
-def get_area(k):
-    return k*k+(k+1)*(k+1)
+space=[list(map(int,input().split())) for _ in range(n)]
 
-def get_num_of_gold(row,col,k):
-    nm=0
+
+def get_num_of_gold(space,row,col,k):
+    cnt=0
     for i in range(n):
         for j in range(n):
-            if abs(row-i)+abs(col-j)<=k:
-                nm+=grid[i][j]
-    return nm
+            if abs(i-row)+abs(j-col)<=k:
+                cnt+=space[i][j]
+    return cnt
+    
 
-max_gold = 0 
-
+answer=0
 for row in range(n):
     for col in range(n):
         for k in range(n+1):
-            num_of_gold=get_num_of_gold(row,col,k)
-            if num_of_gold *m >=get_area(k):
-                max_gold=max(max_gold,num_of_gold)
-
-print(max_gold)
+            if get_num_of_gold(space,row,col,k)*m >=(k*k+(k+1)*(k+1)):
+                answer=max(answer,get_num_of_gold(space,row,col,k))
+print(answer)
