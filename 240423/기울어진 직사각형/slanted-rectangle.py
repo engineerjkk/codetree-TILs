c51aca83e3bbd5c33ca0169427e53207b2acba54
JuneKoo Kang <engineerjkk@naver.com>
2024-04-23 16:18:40 +0900

    Title: 기울어진 직사각형, Time: 266ms, Memory: 27MB, Status: Passed - CodeTree

--- 240423/기울어진 직사각형/slanted-rectangle.py
+++ 240423/기울어진 직사각형/slanted-rectangle.py
@@ -1,21 +1,20 @@
 import sys
 input = sys.stdin.readline
-
-n = int(input())
+n=int(input())
 space=[]
 for _ in range(n):
     space.append(list(map(int,input().split())))
 
-def in_range(r,c):
-    return -1<r<n and -1<c<n
+def in_range(x,y):
+    return -1<x<n and -1<y<n
 
 def score(i,j,k,l):
     drs=[-1,-1,1,1]
     dcs=[1,-1,-1,1]
-    move_nums=[k,l,k,l]
+    moves=[k,l,k,l]
     nm=0
-    for dr,dc,move_num in zip(drs,dcs,move_nums):
-        for _ in range(move_num):
+    for dr,dc, move in zip(drs,dcs,moves):
+        for num in range(move):
             i=i+dr
             j=j+dc
             if not in_range(i,j):
@@ -23,11 +22,11 @@ def score(i,j,k,l):
             nm+=space[i][j]
     return nm
 
-answer=0
+ans=0
 for i in range(n):
     for j in range(n):
         for k in range(1,n):
             for l in range(1,n):
-                value=score(i,j,k,l)
-                answer=max(answer,value)
-print(answer)
+                value = score(i,j,k,l)
+                ans=max(ans,value)
+print(ans)
