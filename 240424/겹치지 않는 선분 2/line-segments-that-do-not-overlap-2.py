7b2ca05ee8f8dd576f7f379648e6ebce46d03e53
JuneKoo Kang <engineerjkk@naver.com>
2024-04-24 00:14:32 +0900

    Title: 겹치지 않는 선분 2, Time: 143ms, Memory: 40MB, Status: Wrong Answer - CodeTree

---
+++ 240424/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+lines=[]
+mil=1000000
+check=[0]*(mil*2+1)
+for _ in range(n):
+    lines.append(list(map(int,input().split())))
+
+lines.sort(key=lambda x:x[0])
+
+def check_point(start,end):
+    for i in range(start,end):
+        check[i]+=1
+
+    for i in range(start,end):
+        if check[i]>=2:
+            return False
+    return True
+
+def mask(start,end):
+    for i in range(start,end):
+        check[i]=1
+    return 1
+
+ans=0
+for i in range(n):
+    start,end=lines[i]
+    start=start+mil
+    end=end+mil
+    if check_point(start,end+1):
+        ans+=mask(start,end+1)
+print(ans)
