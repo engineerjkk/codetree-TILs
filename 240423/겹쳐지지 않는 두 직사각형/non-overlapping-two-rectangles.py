23b4b6e6b4dcda5b8476c1b1cdca4bbfe2831059
JuneKoo Kang <engineerjkk@naver.com>
2024-04-23 18:13:12 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 783ms, Memory: 29MB, Status: Passed - CodeTree

--- 240423/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240423/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,15 +1,11 @@
 import sys
-INT_MIN = - sys.maxsize
-n,m = tuple(map(int,input().split()))
-grid = [
-    list(map(int,input().split()))
-    for _ in range(n)
-]
+INT_MIN = -sys.maxsize
+n,m = map(int,input().split())
+grid =[]
+for _ in range(n):
+    grid.append(list(map(int,input().split())))
 
-board=[
-    [0 for _ in range(m)]
-    for _ in range(n)
-]
+board=[[0]*m for _ in range(n)]
 
 def clear_board():
     for i in range(n):
@@ -24,7 +20,7 @@ def draw(x1,y1,x2,y2):
 def check_board():
     for i in range(n):
         for j in range(m):
-            if board[i][j] >=2:
+            if board[i][j]>=2:
                 return True
     return False
 
@@ -35,14 +31,15 @@ def overlapped(x1,y1,x2,y2,x3,y3,x4,y4):
     return check_board()
 
 def rect_sum(x1,y1,x2,y2):
-    return sum([
-        grid[i][j]
-        for i in range(x1,x2+1)
-        for j in range(y1,y2+1) 
-    ])
+    nm=0
+    for i in range(x1,x2+1):
+        for j in range(y1,y2+1):
+            nm+=grid[i][j]
+    return nm
+
 
 def find_max_sum_with_rect(x1,y1,x2,y2):
-    max_sum=INT_MIN
+    max_sum = INT_MIN
     for i in range(n):
         for j in range(m):
             for k in range(i,n):
@@ -57,7 +54,7 @@ def find_max_sum():
         for j in range(m):
             for k in range(i,n):
                 for l in range(j,m):
-                    max_sum = max(max_sum,find_max_sum_with_rect(i,j,k,l))
+                    max_sum=max(max_sum,find_max_sum_with_rect(i,j,k,l))
     return max_sum
 
 ans=find_max_sum()
