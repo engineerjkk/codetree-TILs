c41db778d13e4a503e9dd9f750a66fecc12dbbb6
JuneKoo Kang <engineerjkk@naver.com>
2024-04-23 22:22:42 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 899ms, Memory: 29MB, Status: Passed - CodeTree

--- 240423/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240423/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,61 +1,58 @@
 import sys
-INT_MIN = -sys.maxsize
-n,m = map(int,input().split())
-grid =[]
+input = sys.stdin.readline
+n,m= map(int,input().split())
+space=[]
 for _ in range(n):
-    grid.append(list(map(int,input().split())))
+    space.append(list(map(int,input().split())))
 
+ans=-sys.maxsize
 board=[[0]*m for _ in range(n)]
 
-def clear_board():
+def clearboard():
     for i in range(n):
         for j in range(m):
             board[i][j]=0
 
-def draw(x1,y1,x2,y2):
+def draw_rec(x1,y1,x2,y2):
     for i in range(x1,x2+1):
         for j in range(y1,y2+1):
             board[i][j]+=1
-
-def check_board():
+def checkboard():
     for i in range(n):
         for j in range(m):
             if board[i][j]>=2:
-                return True
-    return False
+                return False
+    return True
+
+
 
 def overlapped(x1,y1,x2,y2,x3,y3,x4,y4):
-    clear_board()
-    draw(x1,y1,x2,y2)
-    draw(x3,y3,x4,y4)
-    return check_board()
+    clearboard()
+    draw_rec(x1,y1,x2,y2)
+    draw_rec(x3,y3,x4,y4)
+    return checkboard()
 
-def rect_sum(x1,y1,x2,y2):
+def rec(x1,y1,x2,y2):
     nm=0
     for i in range(x1,x2+1):
         for j in range(y1,y2+1):
-            nm+=grid[i][j]
+            nm+=space[i][j]
     return nm
 
 
-def find_max_sum_with_rect(x1,y1,x2,y2):
-    max_sum = INT_MIN
+def find_max_rec(x1,y1,x2,y2):
+    ans2=-sys.maxsize
     for i in range(n):
         for j in range(m):
             for k in range(i,n):
                 for l in range(j,m):
-                    if not overlapped(x1,y1,x2,y2,i,j,k,l):
-                        max_sum = max(max_sum,rect_sum(x1,y1,x2,y2)+rect_sum(i,j,k,l))
-    return max_sum
+                    if overlapped(i,j,k,l,x1,y1,x2,y2):
+                        ans2=max(ans2,rec(i,j,k,l)+rec(x1,y1,x2,y2))
+    return ans2
 
-def find_max_sum():
-    max_sum=INT_MIN
-    for i in range(n):
-        for j in range(m):
-            for k in range(i,n):
-                for l in range(j,m):
-                    max_sum=max(max_sum,find_max_sum_with_rect(i,j,k,l))
-    return max_sum
-
-ans=find_max_sum()
+for i in range(n):
+    for j in range(m):
+        for k in range(i,n):
+            for l in range(j,m):
+                ans=max(ans,find_max_rec(i,j,k,l))
 print(ans)
