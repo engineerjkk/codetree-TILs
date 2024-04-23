c8f13fc5c4ab7c796152eb9262daca452997a043
JuneKoo Kang <engineerjkk@naver.com>
2024-04-23 12:34:03 +0900

    Title: 금 채굴하기, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240423/금 채굴하기/gold-mining.py
+++ 240423/금 채굴하기/gold-mining.py
@@ -8,18 +8,18 @@ def get_area(k):
     return k*k+(k+1)*(k+1)
 
 def get_num_of_gold(row,col,k):
-    return sum([
-        grid[i][j]
-        for i in range(n)
-        for j in range(n)
-        if abs(row-i)+abs(col-j)<=k
-    ])
+    nm=0
+    for i in range(n):
+        for j in range(n):
+            if abs(row-i)+abs(col-j)<=k:
+                nm+=grid[i][j]
+    return nm
 
 max_gold = 0 
 
 for row in range(n):
     for col in range(n):
-        for k in range(2*(n-1)+1):
+        for k in range(n-1):
             num_of_gold=get_num_of_gold(row,col,k)
             if num_of_gold *m >=get_area(k):
                 max_gold=max(max_gold,num_of_gold)
