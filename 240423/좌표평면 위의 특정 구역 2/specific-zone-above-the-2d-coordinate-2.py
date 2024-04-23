f5f0ff5eb011248b9def1e5e66108d6c6332d541
JuneKoo Kang <engineerjkk@naver.com>
2024-04-23 21:59:44 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240423/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,25 @@
+import sys
+input= sys.stdin.readline
+n=int(input())
+space=[]
+for _ in range(n):
+    space.append(list(map(int,input().split())))
+
+
+ans=1000000
+for i in range(n):
+    row=[]
+    col=[]
+    for j in range(n):
+        if i==j:
+            continue
+        x,y=space[j]
+        row.append(x)
+        col.append(y)
+    x_max=max(row)
+    x_min=min(row)
+    y_max=max(col)
+    y_min=min(col)
+    rec=(x_max-x_min)*(y_max-y_min)
+    ans=min(ans,rec)
+print(ans)
