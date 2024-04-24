623a134df1efe4514c57e8433606e4dcab18ed72
JuneKoo Kang <engineerjkk@naver.com>
2024-04-24 16:02:28 +0900

    Title: 스승의 은혜 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240424/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+n,b = map(int,input().split())
+student=[]
+for _ in range(n):
+    student.append(int(input()))
+
+
+
+ans=0
+for i in range(n):
+    for j in range(n):
+        for k in range(i,j+1):
+            cash=0
+            for l in range(i,k):
+                if l==k:
+                    cash+=student[l]//2
+                else:
+                    cash+=student[l]
+            if b>=cash:
+                ans=max(ans,k)
+print(ans)
