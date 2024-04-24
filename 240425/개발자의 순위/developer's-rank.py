897e54677c7c4047a82207558e31f2da2c9578af
JuneKoo Kang <engineerjkk@naver.com>
2024-04-25 01:19:27 +0900

    Title: 개발자의 순위, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240425/개발자의 순위/developer's-rank.py
+++ 240425/개발자의 순위/developer's-rank.py
@@ -5,22 +5,37 @@ k,n = map(int,input().split())
 lst=[]
 for _ in range(k):
     lst.append(list(map(int,input().split())))
-
-arr=[[] for _ in range(21)]
-
-for i in range(k):
-    for j in range(n):
-        for l in range(n):
-            if i==l: 
-                continue
-            elif lst[i][j]>lst[i][l]:
-                arr[j+1].append(lst[i][l])
-print(arr)
-cnt=0
-for i in range(n+1):
-    for j in range(n+1):
+ans=0
+for i in range(1,n+1):
+    for j in range(1, n+1):
         if i==j:
             continue
-        if arr[i+1].count(j)==k:
-            cnt+=1
-print(cnt)
+        correct = True
+
+        for ls in lst:
+            index_i=ls.index(i)
+            index_j=ls.index(j)
+            if index_i>index_j:
+                correct=False
+        if correct:
+            ans+=1
+print(ans)
+
+# arr=[[] for _ in range(21)]
+
+# for i in range(k):
+#     for j in range(n):
+#         for l in range(n):
+#             if i==l: 
+#                 continue
+#             elif lst[i][j]>lst[i][l]:
+#                 arr[j+1].append(lst[i][l])
+# print(arr)
+# cnt=0
+# for i in range(n+1):
+#     for j in range(n+1):
+#         if i==j:
+#             continue
+#         if arr[i+1].count(j)==k:
+#             cnt+=1
+# print(cnt)
