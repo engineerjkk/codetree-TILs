e24d2e7efb996bd4bbbb6d06c4301a3ffd18c1e5
JuneKoo Kang <engineerjkk@naver.com>
2024-04-24 23:58:51 +0900

    Title: 이상한 폭탄 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240424/이상한 폭탄 2/strange-bomb-2.py
+++ 240424/이상한 폭탄 2/strange-bomb-2.py
@@ -1,16 +1,15 @@
 # import sys
 # input = sys.stdin.readline
-n,k = map(int,input().split())
+n,k = tuple(map(int,input().split()))
 lst=[]
 for _ in range(n):
     lst.append(int(input()))
 
 ans=0
-tmp=[]
-for i in range(n):
-    for j in range(n):
-        if i==j:
-            continue
+for i in range(n-1):
+    for j in range(i+1,n):
+        # if i==j:
+        #     continue
         if lst[i]==lst[j]:
             if lst[i]>=abs(i-j):
                 ans=max(ans,lst[i])
