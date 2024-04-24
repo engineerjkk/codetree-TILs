db96a36206b494d926cd8a8ac424a57564fbb5b1
JuneKoo Kang <engineerjkk@naver.com>
2024-04-24 16:41:36 +0900

    Title: 스승의 은혜 2, Time: 248ms, Memory: 26MB, Status: Passed - CodeTree

--- 240424/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240424/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,6 +1,6 @@
 import sys
 input = sys.stdin.readline
-n,b = map(int,input().split())
+n,b=map(int,input().split())
 student=[]
 for _ in range(n):
     student.append(int(input()))
@@ -10,17 +10,14 @@ for i in range(n):
     tmp=[]
     for j in range(n):
         tmp.append(student[j])
-    tmp.sort()
-
     tmp[i]=tmp[i]/2
-    
-    cnt=0
+    tmp.sort()
     cash=0
+    cnt=0
     for j in range(n):
         if cash+tmp[j]>b:
             break
         cash+=tmp[j]
         cnt+=1
     ans=max(ans,cnt)
-        
 print(ans)
