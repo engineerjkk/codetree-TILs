990bbb42c00d39e61c6d4ee1ea53bcfb3bc20754
JuneKoo Kang <engineerjkk@naver.com>
2024-04-24 21:10:55 +0900

    Title: 스승의 은혜 2, Time: 109ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240424/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240424/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,19 +1,20 @@
 import sys
-input = sys.stdin.readline
-n,b = map(int,input().split())
-student=[]
+input=sys.stdin.readline
+n,b=map(int,input().split())
+students=[]
 for _ in range(n):
-    student.append(int(input()))
+    students.append(int(input()))
+
 ans=0
 for i in range(n):
+
     tmp=[]
     for j in range(n):
-        tmp.append(student[j])
+        tmp.append(students[j])
     tmp[i]=tmp[i]/2
-    
+
     cnt=0
     cash=0
-    tmp.sort()
     for j in range(n):
         if cash+tmp[j]>b:
             break
