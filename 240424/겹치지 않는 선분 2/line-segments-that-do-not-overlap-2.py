ef1c3a587cda5783ea0c7a13da93d1e405871c3e
JuneKoo Kang <engineerjkk@naver.com>
2024-04-24 14:59:54 +0900

    Title: 겹치지 않는 선분 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240424/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240424/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,21 +1,18 @@
 import sys
-input = sys.stdin.readline
-n = int(input())
-
+input= sys.stdin.readline
+n=int(input())
 lines=[]
 for _ in range(n):
     lines.append(list(map(int,input().split())))
-ans=0
 
 for i in range(n):
-    overlap=False
+    cnt=0
     for j in range(n):
+        overlapped=False
         if i==j:
             continue
         
-        if (lines[i][0]<=lines[j][0] and lines[i][1] >=lines[j][1]) or (lines[i][0] >=lines[j][0] and lines[i][1]<=lines[j][1]):
-            overlap = True
-            break
-    if overlap == False:
-        ans+=1
-print(ans)
+        if (lines[i][0] <=lines[j][0] and lines[i][1] >=lines[j][1]) or (lines[i][0] >= lines[j][0] and lines[i][0] <= lines[j][1]):
+            overlapped=True
+            cnt+=1
+print(cnt)
