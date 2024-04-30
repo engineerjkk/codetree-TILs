71735f289979f6ee7e49a4ca41ab9682b282a0d1
JuneKoo Kang <engineerjkk@naver.com>
2024-05-01 08:47:02 +0900

    Title: Date to Date, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/Date to Date/date-to-date.py
+++ 240501/Date to Date/date-to-date.py
@@ -6,13 +6,13 @@ year=[31,28,31,30,31,30,31,31,30,31,30,31]
 
 month=m1
 day=d1
-ans=0
+ans=1
 while True:
     if month==m2 and day==d2:
         break
     ans+=1
     day+=1
-    if day==year[month-1]:
+    if day>year[month-1]:
         month+=1
         day=1
-print(ans+2)
+print(ans)
