f92ed23ffd537d2600b7d2f702184d9bf705b935
JuneKoo Kang <engineerjkk@naver.com>
2024-04-30 21:28:59 +0900

    Title: 스승의 은혜 3, Time: 656ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240430/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240430/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -11,13 +11,13 @@ ans=0
 cnt=0
 #lst.sort()
 for i in range(n):
-    cash=0
-    cnt=0
+
     tmp=[]
     for j in range(n):
         tmp.append(lst[j])
-    tmp[i][0]=lst[i][0]/2
-    tmp.sort()
+    tmp[i][0]=tmp[i][0]/2
+    cash=0
+    cnt=0
     tmp_price = sorted(tmp, key=lambda x: x[0])
     for x in range(n):
         if cash+tmp_price[x][0]+tmp_price[x][1]>b:
@@ -30,6 +30,7 @@ for i in range(n):
 ###
     tmp_cheap = sorted(tmp, key=lambda x: x[1])
     cnt=0
+    cash=0
     for x in range(n):
         if cash+tmp_cheap[x][0]+tmp_cheap[x][1]>b:
             break
