72b4f8a0474943c2a05aee8c9f4d09e24e7b7471
JuneKoo Kang <engineerjkk@naver.com>
2024-04-30 22:16:55 +0900

    Title: 스승의 은혜 3, Time: 148ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240430/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240430/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,30 +1,32 @@
 import sys
 input = sys.stdin.readline
+import copy
 n,b = map(int,input().split())
 lst=[]
 for _ in range(n):
     lst.append(list(map(int,input().split())))
 
-
-
 ans=0
 cnt=0
-#lst.sort()
 for i in range(n):
-    tmp=[]
-    for j in range(n):
-        tmp.append(lst[j])
-    tmp[i][0]=tmp[i][0]/2
+    tmp=copy.deepcopy(lst)
+    # tmp=[]
+    # for j in range(n):
+    #     tmp.append(lst[j])
+    
+    tmp[i][0]=tmp[i][0]//2
     cash=0
     cnt=0
-    tmp_price = sorted(tmp, key=lambda x: x[0])
+    #tmp_price = sorted(tmp, key=lambda x: x[0])
+    tmp.sort()
     for x in range(n):
-        if cash+tmp_price[x][0]+tmp_price[x][1]>b:
+        if cash+tmp[x][0]+tmp[x][1]>b:
             break
-        cash+=tmp_price[x][0]
-        cash+=tmp_price[x][1]
+        cash+=tmp[x][0]
+        cash+=tmp[x][1]
         cnt+=1
-    ans=max(ans,cnt)
+    if cnt>ans:
+        ans=cnt
 
     # tmp_cheap = sorted(tmp, key=lambda x: x[1])
     # cnt=0
