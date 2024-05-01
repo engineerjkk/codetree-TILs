abb5064f45ac3d872bfa27d0a8267549e4680a4a
JuneKoo Kang <engineerjkk@naver.com>
2024-05-01 23:22:58 +0900

    Title: 만나는 그 순간, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,51 @@
+n,m=map(int,input().split())
+lst_A=[]
+for _ in range(n):
+    lst_A.append(list(map(str,input().split())))
+
+lst_B=[]
+for _ in range(m):
+    lst_B.append(list(map(str,input().split())))
+
+tmp_A=[0]*1001
+tmp_B=[0]*1001
+i=0
+cnt_A=0
+pos_A=0
+for i in range(n):
+    direction, move= lst_A[i]
+    move=int(move)
+    if direction=='R':
+        for j in range(1,move+1):
+            cnt_A+=1
+            pos_A+=1
+            tmp_A[cnt_A]=pos_A
+            
+    elif direction=='L':
+        for j in range(1,move+1):
+            pos_A-=1
+            cnt_A+=1
+            tmp_A[cnt_A]=pos_A
+
+cnt_B=0
+pos_B=0
+for i in range(m):
+    direction, move= lst_B[i]
+    move=int(move)
+    if direction=='R':
+        for j in range(1,move+1):
+            cnt_B+=1
+            pos_B+=1
+            tmp_B[cnt_B]=pos_B
+
+            
+    elif direction=='L':
+        for j in range(1,move+1):
+            pos_B-=1
+            cnt_B+=1
+            tmp_B[cnt_B]=pos_B
+
+for i in range(1,len(tmp_A)):
+    if tmp_A[i]==tmp_B[i]:
+        print(i)
+        exit()
