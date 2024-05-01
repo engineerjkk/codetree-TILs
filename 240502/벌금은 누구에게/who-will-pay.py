a8e510062f5d65a88e5b05c6ad0488b450520134
JuneKoo Kang <engineerjkk@naver.com>
2024-05-02 00:21:07 +0900

    Title: 벌금은 누구에게, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240502/벌금은 누구에게/who-will-pay.py
+++ 240502/벌금은 누구에게/who-will-pay.py
@@ -3,13 +3,20 @@ lst=[]
 ans=False
 for _ in range(m):
     lst.append(int(input()))
-tmp=[0]
+tmp=[0]*n
 for i in lst:
     tmp.append(i)
-for i in range(k):
-    for j,num in enumerate(lst):
-        tmp[num]-=1
-        if tmp[num]==0:
-            print(num)
-            exit()
+for j,num in enumerate(lst):
+    tmp[num]+=1
+    if tmp[num]>=k:
+        print(num)
+        exit()
 print(-1)
+    
+# for i in range(k):
+#     for j,num in enumerate(lst):
+#         tmp[num]-=1
+#         if tmp[num]==0:
+#             print(num)
+#             exit()
+# print(-1)
