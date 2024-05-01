c0477156d243d114170781c5271daedd69755f0b
JuneKoo Kang <engineerjkk@naver.com>
2024-05-01 09:24:07 +0900

    Title: 그 요일은, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+m1,d1,m2,d2=map(int,input().split())
+A=str(input())
+year=[31,29,31,30,31,30,31,31,30,31,30,31]
+DayOfWeek=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+
+month=m1
+day=d1
+DoW=0
+ans=0
+while True:
+    if month==m2 and day==d2:
+        print(ans)
+        exit()
+    day+=1
+    DoW+=1
+    if day>year[month-1]:
+        day=1
+        month+=1
+    if DoW>6:
+        DoW=0
+    if DayOfWeek[DoW]==A:
+        ans+=1
+
+
+# month=m1
+# day=d1
+# time=0
+# DoW=0
+# ans=0
+# while True:
+#     if month==m2 and day==d1:
+#         print(ans)
+#         exit()
+#     if month==1 and day==1:
+#         break
+#     day-=1
+#     if day<1:
+#         month-=1
+#         day=year[month-1]
+#     DoW-=1
+#     if DoW<0:
+#         DoW=6
+#     if DayOfWeek[DoW]==A:
+#         ans+=1
