23c1fc4a6d314d837d5ce39859540648b72ebd21
JuneKoo Kang <engineerjkk@naver.com>
2024-05-03 15:41:28 +0900

    Title: 두 수의 합, Time: 5074ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 240503/두 수의 합/sum-of-two-num.py
+++ 240503/두 수의 합/sum-of-two-num.py
@@ -4,8 +4,10 @@ n, k = map(int,input().split())
 dic={}
 lst=list(map(int,input().split()))
 ans=0
+for i in range(n):
+    dic[i]=lst[i]
 for i in range(n-1):
     for j in range(i+1,n):
-        if lst[i]+lst[j]==k:
+        if dic[i]+dic[j]==k:
             ans+=1
 print(ans)
