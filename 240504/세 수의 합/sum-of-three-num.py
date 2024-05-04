36b8d5d536d09f54b4e3f2a6633b02db6ac259d9
JuneKoo Kang <engineerjkk@naver.com>
2024-05-04 20:40:24 +0900

    Title: 세 수의 합, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240504/세 수의 합/sum-of-three-num.py
+++ 240504/세 수의 합/sum-of-three-num.py
@@ -1,20 +1,19 @@
 import sys
 input = sys.stdin.readline
 n, k = map(int,input().split())
-arr=list(map(int,input().split()))
+lst=list(map(int,input().split()))
+
 count={}
-for elem in arr:
+for elem in lst:
     if elem in count:
         count[elem]+=1
     else:
         count[elem]=1
-    
 ans=0
-
 for i in range(n):
-    count[arr[i]]-=1
+    count[lst[i]]-=1
     for j in range(i):
-        diff=k-arr[i]-arr[j]
+        diff=k-lst[i]-lst[j]
         if diff in count:
             ans+=count[diff]
 print(ans)
