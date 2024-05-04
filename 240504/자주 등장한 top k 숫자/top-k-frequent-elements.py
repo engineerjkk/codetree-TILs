e654474b95ab7537b6eb5d79d39f910bb3bb82cb
JuneKoo Kang <engineerjkk@naver.com>
2024-05-04 22:15:45 +0900

    Title: 자주 등장한 top k 숫자, Time: 179ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240504/자주 등장한 top k 숫자/top-k-frequent-elements.py
+++ 240504/자주 등장한 top k 숫자/top-k-frequent-elements.py
@@ -4,27 +4,48 @@ n, k = map(int,input().split())
 lst = list(map(int,input().split()))
 
 count={}
-for elem in lst:
-    if elem in count:
-        count[elem]+=1
+for i in lst:
+    if i in count:
+        count[i]+=1
     else:
-        count[elem]=1
-ans_1st=0
+        count[i]=1
+sorted(count.items(), key = lambda item: item[1],reverse=True)
+answer={}
+for key, value in count.items():
+    if value in answer:
+        answer[value].append(key)
+    else:
+        answer[value]=[key]
+count={}
 ans_lst=[]
-ans_cnt=[]
-ans_cnt=0
-ans_value=0
-check=0
-for i in range(k):
-    ans_cnt=0
-    ans_value=0
-    for j in range(n):
-        if lst[j] in count:
-            if count[lst[j]] >= ans_cnt and lst[j] >= ans_value:
-                ans_cnt=count[lst[j]]
-                ans_value=lst[j]
-    count.pop(ans_value,None)
-    ans_lst.append(ans_value)
+cnt=0
 
-for i in ans_lst:
-    print(i,end=" ")
+while True:
+    for key, value in answer.items():
+        tmp_lst=[]
+        for i in value:
+            tmp_lst.append(i)
+        tmp_lst=sorted(tmp_lst,reverse=True)
+        for i in tmp_lst:
+            print(i,end=" ")
+            cnt+=1
+            if cnt==k:
+                exit()
+
+    
+
+# ans_cnt=0
+# ans_value=0
+# ans_lst=[]
+# for i in range(k):
+#     ans_cnt=0
+#     ans_value=0
+#     for j in lst:
+#         if j in count and j >= ans_cnt and lst[j] >= ans_value:
+#             ans_cnt=count[lst[j]]
+#             ans_value=lst[j]
+#     count.pop(ans_value,None)
+#     ans_lst.append(ans_value)
+
+# for i in ans_lst:
+#     print(i,end=" ")
