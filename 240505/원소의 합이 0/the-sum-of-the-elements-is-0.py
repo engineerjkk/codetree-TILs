691487aa17db85d3772aaacd29b7748dbd3e3c6a
JuneKoo Kang <engineerjkk@naver.com>
2024-05-05 14:06:07 +0900

    Title: 원소의 합이 0, Time: 193ms, Memory: 39MB, Status: Passed - CodeTree

---
+++ 240505/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,22 @@
+n= int(input())
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+C=list(map(int,input().split()))
+D=list(map(int,input().split()))
+
+count={}
+ans=0
+
+for i in range(n):
+    for j in range(n):
+        sum_val=A[i]+B[j]
+        if sum_val in count:
+            count[sum_val]+=1
+        else:
+            count[sum_val]=1
+for i in range(n):
+    for j in range(n):
+        diff = -C[i] - D[j]
+        if diff in count:
+            ans += count[diff]
+print(ans)
