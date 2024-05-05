ac472b4f5f2b36bd901298089d0d6c271e6bb63b
JuneKoo Kang <engineerjkk@naver.com>
2024-05-05 14:56:23 +0900

    Title: 특별한 문자, Time: 179ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240505/특별한 문자/special-character.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+lst=list(map(str,input().strip()))
+dic={}
+for i in lst:
+    if i in dic:
+        dic[i]+=1
+    else:
+        dic[i]=1
+sorted_dic=sorted(dic.items(),key=lambda x:x[1])
+if sorted_dic[0][1]!=1:
+    print("None")
+else:
+    for i in lst:
+        for key,value in sorted_dic:
+            if key==i and value==1:
+                print(key)
+                exit()
