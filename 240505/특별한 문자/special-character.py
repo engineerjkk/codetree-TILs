817c1fe24efd66edbc2aabe2df6a4abfa5d6dab2
JuneKoo Kang <engineerjkk@naver.com>
2024-05-05 14:57:28 +0900

    Title: 특별한 문자, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240505/특별한 문자/special-character.py
+++ 240505/특별한 문자/special-character.py
@@ -7,12 +7,17 @@ for i in lst:
         dic[i]+=1
     else:
         dic[i]=1
-sorted_dic=sorted(dic.items(),key=lambda x:x[1])
-if sorted_dic[0][1]!=1:
-    print("None")
-else:
-    for i in lst:
-        for key,value in sorted_dic:
-            if key==i and value==1:
-                print(key)
-                exit()
+
+for elem in lst:
+    if dic[elem]==1:
+        print(elem)
+        exit()
+# sorted_dic=sorted(dic.items(),key=lambda x:x[1])
+# if sorted_dic[0][1]!=1:
+#     print("None")
+# else:
+#     for i in lst:
+#         for key,value in sorted_dic:
+#             if key==i and value==1:
+#                 print(key)
+#                 exit()
