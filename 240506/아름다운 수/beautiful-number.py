6f5f3d626e49ba14679a9f00eec2d43007050ef9
JuneKoo Kang <engineerjkk@naver.com>
2024-05-06 12:26:10 +0900

    Title: 아름다운 수, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240506/아름다운 수/beautiful-number.py
+++ 240506/아름다운 수/beautiful-number.py
@@ -8,12 +8,11 @@ for i in range(1,n+1):
 nCr=product(lst,repeat=n)
 
 ans=0
-for i in nCr:
+for x in nCr:
     tmp=True
-    for j in (1,n+1):
-        if j in i:
-            if i.count(j)%j!=0:
-                tmp=False
+    for j in x:
+        if x.count(j)%j!=0:
+            tmp=False
     if tmp:
         ans+=1            
 print(ans)
