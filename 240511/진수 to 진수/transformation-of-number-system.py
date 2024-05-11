0b9b666256656e504046956a5b399bbdc2f54139
JuneKoo Kang <engineerjkk@naver.com>
2024-05-11 11:52:32 +0900

    Title: 진수 to 진수, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240511/진수 to 진수/transformation-of-number-system.py
+++ 240511/진수 to 진수/transformation-of-number-system.py
@@ -3,15 +3,12 @@ input = sys.stdin.readline
 a,b = map(int,input().split())
 value = str(input())
 
-def solution(n, q):
-    rev_base = ''
+tmp=int(value,a)
 
-    while n > 0:
-        n, mod = divmod(n, q)
-        rev_base += str(mod)
-
-    return rev_base[::-1] 
-    # 역순인 진수를 뒤집어 줘야 원래 변환 하고자하는 base가 출력
-tmp = int(value,a)
-
-print(solution(tmp, b))
+def solution(n,q):
+    rev_base=''
+    while n>0:
+        n,mod=divmod(n,q)
+        rev_base+=str(mod)
+    return rev_base[::-1]
+print(solution(tmp,b))
