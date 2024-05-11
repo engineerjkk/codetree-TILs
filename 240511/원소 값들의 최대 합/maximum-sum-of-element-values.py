2a71896dcbf123e9b2ccd047f8551e68cab7d1fe
JuneKoo Kang <engineerjkk@naver.com>
2024-05-11 10:41:16 +0900

    Title: 원소 값들의 최대 합, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 240511/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 240511/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -1,18 +1,30 @@
-import sys
-input = sys.stdin.readline
-from collections import deque
-n,m = map(int,input().split())
-lst=list(map(int,input().split()))
+# import sys
+# input = sys.stdin.readline
+# from collections import deque
+# n,m = map(int,input().split())
+# lst=list(map(int,input().split()))
 
+# ans=0
+# for i in range(n):
+#     arr=[]
+#     queue=deque()
+#     queue.append(lst[i])
+#     for j in range(m):
+#         q=queue.popleft()
+#         arr.append(q)
+#         queue.append(lst[q-1])
+#     value=sum(arr)
+#     ans=max(ans,value)
+# print(ans)
+        
+n,m = map(int,input().split())
+arr=list(map(int,input().split()))
 ans=0
 for i in range(n):
-    arr=[]
-    queue=deque()
-    queue.append(lst[i])
-    for j in range(m):
-        q=queue.popleft()
-        arr.append(q)
-        queue.append(lst[q-1])
-    value=sum(arr)
-    ans=max(ans,value)
+    sum_element=0
+    cur=i
+    for _ in range(m):
+        sum_element +=arr[cur]
+        cur=arr[cur]-1
+    ans=max(ans,sum_element)
 print(ans)
