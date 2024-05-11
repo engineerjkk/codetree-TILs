f608f24fc413fbe06ae2b6082cc5290db486c874
JuneKoo Kang <engineerjkk@naver.com>
2024-05-11 13:37:12 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240511/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+n=int(input())
+lst=[]
+for _ in range(n):
+    lst.append(list(map(str,input().split())))
+
+arr=[0]*101
+
+current=50
+for i in range(n):
+    cnt,direction = lst[i]
+    cnt=int(cnt)
+    if direction == 'R':
+        arr[current]=1
+        for j in range(cnt-1):
+            current+=1
+            arr[current]=1
+            
+    else:
+        arr[current]=-1
+        for j in range(cnt-1):
+            current-=1
+            arr[current]=-1
+print(arr.count(-1),end=" ")
+print(arr.count(1))
