1f9814ec3c4c2449de6de204517492590dc7c547
JuneKoo Kang <engineerjkk@naver.com>
2024-05-11 15:32:43 +0900

    Title: 줄 세우기 2, Time: 168ms, Memory: 26MB, Status: Passed - CodeTree

--- 240511/줄 세우기 2/line-up-students-2.py
+++ 240511/줄 세우기 2/line-up-students-2.py
@@ -6,9 +6,11 @@ for _ in range(n):
     lst.append(list(map(int,input().split())))
 
 tmp=sorted(lst,key=lambda x:(x[0],-x[1]))
+ans=[]
 for i in range(n):
-    tmp[i].append(i+1)
-for i in tmp:
+    idx=lst.index(tmp[i])
+    ans.append([tmp[i][0],tmp[i][1],idx+1])
+for i in ans:
     for j in i:
         print(j,end=" ")
     print()
