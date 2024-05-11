d64c1e49841e083235bc84e5ade6a64582b1b2ce
JuneKoo Kang <engineerjkk@naver.com>
2024-05-11 15:00:29 +0900

    Title: 중앙값 계산 2, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240511/중앙값 계산 2/get-median-2.py
+++ 240511/중앙값 계산 2/get-median-2.py
@@ -6,8 +6,8 @@ lst=list(map(int,input().split()))
 tmp=[]
 for i in range(n):
     tmp.append(lst[i])
-    if i==0 or i%2==1:
-        sorted(tmp)
+    if i==0 or i%2==0:
+        tmp.sort()
         idx=len(tmp)//2
         #print(idx)
         print(tmp[idx],end=" ")
