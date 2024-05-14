172fe97c224f73ed89510256e4b0ef75058c4faa
JuneKoo Kang <engineerjkk@naver.com>
2024-05-14 15:21:22 +0900

    Title: G & H 반전시키기 3, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240514/G & H 반전시키기 3/reversing-g-and-h-3.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+n=int(input())
+start=input()
+end=input()
+
+ans=0
+cnt=0
+mismatched=False
+for i in range(n):
+    if start[i] != end[i]:
+        cnt+=1
+        if mismatched==False:
+            ans+=1
+            mismatched=True
+        if cnt>4:
+            cnt=0
+            ans+=1
+            
+    else:
+        mismatched=False
+print(ans)
