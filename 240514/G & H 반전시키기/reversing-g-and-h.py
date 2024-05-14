9ca6d51b76ef47b8cabc4a0e85271e0226dfc528
JuneKoo Kang <engineerjkk@naver.com>
2024-05-14 15:09:02 +0900

    Title: G & H 반전시키기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240514/G & H 반전시키기/reversing-g-and-h.py
+++ 240514/G & H 반전시키기/reversing-g-and-h.py
@@ -6,9 +6,10 @@ cnt=0
 mismatched=False
 
 for i in range(n):
-    if start[i]!=end[i] and mismatched==False :
-        mismatched=True
-        cnt+=1
+    if start[i]!=end[i]:
+        if mismatched==False :
+            mismatched=True
+            cnt+=1
     else:
         mismatched=False
 print(cnt)
