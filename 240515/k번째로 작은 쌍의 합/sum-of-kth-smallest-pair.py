0e74b945ea8062e29846dec555243a932f247f5a
JuneKoo Kang <engineerjkk@naver.com>
2024-05-15 08:22:14 +0900

    Title: k번째로 작은 쌍의 합, Time: 2431ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240515/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 240515/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -7,10 +7,10 @@ lst_n = list(map(int,input().split()))
 lst_m = list(map(int,input().split()))
 
 lst=[]
+hq=[]
 for i in range(n):
     for j in range(m):
-        lst.append(lst_n[i]+lst_m[j])
-heapq.heapify(lst)
+        heapq.heappush(hq,lst_n[i]+lst_m[j])
 for i in range(k-1):
-    heapq.heappop(lst)
-print(lst[0])
+    heapq.heappop(hq)
+print(hq[0])
