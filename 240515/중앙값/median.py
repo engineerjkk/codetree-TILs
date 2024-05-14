86ef3ed938492159d3f89c736741960dc4bf6ed6
JuneKoo Kang <engineerjkk@naver.com>
2024-05-15 08:07:11 +0900

    Title: 중앙값, Time: 665ms, Memory: 37MB, Status: Passed - CodeTree

--- 240515/중앙값/median.py
+++ 240515/중앙값/median.py
@@ -1,26 +1,26 @@
-import heapq
 import sys
+import heapq
 input = sys.stdin.readline
 n=int(input())
+
 for _ in range(n):
     m=int(input())
     lst=list(map(int,input().split()))
-
     max_heap=[]
     min_heap=[]
     mid=[]
-    for i in range(m):
+    for j in range(m):
         if len(max_heap)==len(min_heap):
-            heapq.heappush(max_heap,-lst[i])
+            heapq.heappush(max_heap,-lst[j])
         else:
-            heapq.heappush(min_heap,lst[i])
+            heapq.heappush(min_heap,lst[j])
         
         if min_heap and -max_heap[0]>min_heap[0]:
-            big=-heapq.heappop(max_heap)
-            small=heapq.heappop(min_heap)
+            big = -heapq.heappop(max_heap)
+            small = heapq.heappop(min_heap)
             heapq.heappush(min_heap,big)
             heapq.heappush(max_heap,-small)
-        if (i+1)%2==1:
+        if (j+1)%2==1:
             mid.append(-max_heap[0])
     for j in range(len(mid)):
         print(mid[j],end=" ")
