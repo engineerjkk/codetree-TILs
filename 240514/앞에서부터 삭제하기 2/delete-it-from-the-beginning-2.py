88d099bbb0aa2adc2a27a8f59307ea682dac5fa8
JuneKoo Kang <engineerjkk@naver.com>
2024-05-14 21:05:14 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5103ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

---
+++ 240514/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+import heapq
+n = int(input())
+lst=list(map(int,input().split()))
+
+for i in range(1,n-2):
+    pq=[]
+    for j in range(i,n):
+        heapq.heappush(pq,lst[j])
+    heapq.heappop(pq)
+    tmp=[]
+    while pq:
+        tmp.append(heapq.heappop(pq))
+print(f"{sum(tmp)/len(tmp):.2f}")
