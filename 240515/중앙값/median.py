d08832578cd235b753cd755f82b5804ae19a13cb
JuneKoo Kang <engineerjkk@naver.com>
2024-05-15 07:40:34 +0900

    Title: 중앙값, Time: 661ms, Memory: 36MB, Status: Passed - CodeTree

--- 240515/중앙값/median.py
+++ 240515/중앙값/median.py
@@ -6,22 +6,22 @@ for _ in range(n):
     lst=list(map(int,input().split()))
     pq=[]
     ans=[]
-    heapleft=[]
-    heapright=[]
+    heapmax=[]
+    heapmin=[]
     mid=[]
     for i in range(m): 
-        if len(heapleft)==len(heapright):
-            heapq.heappush(heapleft,-lst[i])
+        if len(heapmax)==len(heapmin):
+            heapq.heappush(heapmax,-lst[i])
         else:
-            heapq.heappush(heapright,lst[i])
+            heapq.heappush(heapmin,lst[i])
         
-        if heapright and -heapleft[0]>heapright[0]:
-            big=heapq.heappop(heapleft)
-            small=heapq.heappop(heapright)
-            heapq.heappush(heapleft,-small)
-            heapq.heappush(heapright,-big)
+        if heapmin and -heapmax[0]>heapmin[0]:
+            big=-heapq.heappop(heapmax)
+            small=heapq.heappop(heapmin)
+            heapq.heappush(heapmax,-small)
+            heapq.heappush(heapmin,big)
         if (i+1)%2==1:
-            mid.append(-heapleft[0])
+            mid.append(-heapmax[0])
     for i in range(len(mid)):
         print(mid[i],end=" ")
     print()
