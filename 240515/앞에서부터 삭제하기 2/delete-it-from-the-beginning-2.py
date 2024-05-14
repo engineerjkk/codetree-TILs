fa41ea891c24674a5e15583dc63e6f096a452876
JuneKoo Kang <engineerjkk@naver.com>
2024-05-15 06:56:03 +0900

    Title: 앞에서부터 삭제하기 2, Time: 162ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 240515/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240515/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,23 +1,19 @@
-import sys
-input = sys.stdin.readline
 import heapq
-import copy
-n = int(input())
-arr= list(map(int,input().split()))
 
-sum_val=0
-max_avg=0
+n=int(input())
+lst=list(map(int,input().split()))
+
+sum_avg=0
+ans=0
+
+sum_avg+=lst[n-1]
 pq=[]
 
-heapq.heappush(pq,arr[n-1])
-sum_val+=arr[n-1]
-
 for i in range(n-2,0,-1):
-    heapq.heappush(pq,arr[i])
-    sum_val+=arr[i]
-    min_num=pq[0]
-    avg=(sum_val-min_num)/(n-i-1)
-    if max_avg<avg:
-        max_avg=avg
+    heapq.heappush(pq,lst[i])
+    sum_avg+=lst[i]
+    min_value=pq[0]
+    avg=(sum_avg-min_value)/(n-i-1)
 
-print(f"{max_avg:.2f}")
+    ans=max(ans,avg)
+print(f"{ans:.2f}")
