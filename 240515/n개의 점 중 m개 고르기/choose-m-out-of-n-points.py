681fdeeedee2c193b0ec4273e41dc0b717c67fc2
JuneKoo Kang <engineerjkk@naver.com>
2024-05-15 19:02:03 +0900

    Title: n개의 점 중 m개 고르기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240515/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240515/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -9,7 +9,7 @@ for _ in range(n):
     lst.append(list(map(int,input().split())))
 
 nCr=combinations(lst,m)
-ans=sys.maxsize
+ans=0
 
 def dist(x1,x2,y1,y2):
     return ((x1-x2)**2+(y1-y2)**2)**(1/2)
@@ -18,5 +18,6 @@ for x in nCr:
     nXr=combinations(x,2)
     for j in nXr:
         distance=dist(j[0][0],j[1][0],j[0][1],j[1][1])
-        ans=min(ans,distance)
-print(int(ans**2))
+        ans=max(ans,distance)
+    final_ans=min(final_ans,ans)
+print(int(final_ans**2))
