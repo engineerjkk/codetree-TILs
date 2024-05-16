1f4a3924d90b5a474f9bd56715ad090e0d112ee2
JuneKoo Kang <engineerjkk@naver.com>
2024-05-16 14:08:15 +0900

    Title: 외판원 순회, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240516/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+from itertools import permutations
+
+n = int(input())
+lst=[]
+for _ in range(n):
+    lst.append(list(map(int,input().split())))
+arr=[]
+for i in range(1,n):
+    arr.append(i)
+
+nCr=list(permutations(arr,n-1))
+ans=sys.maxsize
+for x in nCr:
+    x=list(x)
+    x.insert(0,0)
+    x.append(0)
+    value=0
+    for i in range(n):
+        r,c=x[i],x[i+1]
+        if r==c:
+            value+=sys.maxsize
+        else:
+            value+=lst[r][c]
+    ans=min(ans,value)
+print(ans)
