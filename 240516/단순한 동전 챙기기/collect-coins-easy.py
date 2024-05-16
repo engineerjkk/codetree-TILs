045a79ecc7221374a2d2a75a04382d09f4245f09
JuneKoo Kang <engineerjkk@naver.com>
2024-05-16 15:27:45 +0900

    Title: 단순한 동전 챙기기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240516/단순한 동전 챙기기/collect-coins-easy.py
+++ 240516/단순한 동전 챙기기/collect-coins-easy.py
@@ -16,10 +16,9 @@ for i in range(n):
         if lst[i][j] =='E':
             end.append([i,j])
 
-ans=sys.maxsize
+ans=10000000000
 for k in range(3,len(arr)+1):
     nCr=list(permutations(arr,k))
-    nCr=sorted(nCr)
     for x in nCr:
         value=abs(start[0][0]-x[0][0])+abs(start[0][1]-x[0][1])
         cnt=1
@@ -28,13 +27,13 @@ for k in range(3,len(arr)+1):
             r2,c2 = x[i]
             a,b=int(lst[r1][c1]),int(lst[r2][c2])
             if a>b:
-                value+=sys.maxsize
+                value+=1000000
             else:
                 value+=abs(r1-r2)+abs(c1-c2)
                 cnt+=1
         value+=abs(x[-1][0]-end[0][0])+abs(x[-1][1]-end[0][1])
         if cnt<3:
-            value+=sys.maxsize
+            value+=10000000
         ans=min(ans,value)
     if ans<100:
         print(ans)
