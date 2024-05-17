e0a72312060a487444c9d0cc892b858780979ac4
JuneKoo Kang <engineerjkk@naver.com>
2024-05-17 15:10:41 +0900

    Title: 아름다운 수, Time: 317ms, Memory: 25MB, Status: Passed - CodeTree

--- 240517/아름다운 수/beautiful-number.py
+++ 240517/아름다운 수/beautiful-number.py
@@ -4,11 +4,12 @@ from itertools import product
 n=int(input())
 
 lst=[]
-for i in range(1,n+1):
+for i in range(1,5):
     lst.append(i)
 
 nCr=product(lst,repeat=n)
 cnt=0
+tmp=[]
 for x in nCr:
     check=True
     i=0
@@ -24,34 +25,3 @@ for x in nCr:
     if check==True:
         cnt+=1
 print(cnt)
-
-
-    #     if x[i]==1:
-    #         i+=1
-    #     elif x[i]==2:
-    #         if n<i+2:
-    #             check=False
-    #         for j in range(i,i+2):
-    #             if x[i] !=2:
-    #                 check=False
-    #                 break
-    #         i+=(j-i+1)
-    #     elif x[i]==3:
-    #         if n<i+3:
-    #             check=False
-    #         for j in range(i,i+3):
-    #             if x[i] !=3:
-    #                 check=False
-    #                 break
-    #         i+=(j-i+1)
-    #     elif x[i]==4:
-    #         if n<i+4:
-    #             check=False
-    #         for j in range(i,i+4):
-    #             if x[i] !=4:
-    #                 check=False
-    #                 break
-    #         i+=(j-i+1)
-    # if check==True:
-    #     print(x)
-    #     cnt+=1
