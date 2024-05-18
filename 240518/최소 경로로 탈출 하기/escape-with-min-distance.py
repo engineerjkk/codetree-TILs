7ae788657951543463dda4c5d7cb44e8c75f3e90
JuneKoo Kang <engineerjkk@naver.com>
2024-05-18 19:24:27 +0900

    Title: 최소 경로로 탈출 하기, Time: 178ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240518/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n,m = tuple(map(int,input().split()))
+lst=[]
+for _ in range(n):
+    lst.append(list(map(int,input().split())))
+
+queue=deque()
+queue.append((0,0,0))
+
+dr=[-1,0,1,0]
+dc=[0,1,0,-1]
+
+visited=[[0]*n for _ in range(n)]
+
+cnt=1
+while queue:
+    r,c,cnt = queue.popleft()
+    cnt+=1
+    for i in range(4):
+        nr=r+dr[i]
+        nc=c+dc[i]
+        if -1<nr<n and -1<nc<n and lst[nr][nc]==1 and visited[nr][nc]==0:
+            visited[nr][nc]=cnt
+            queue.append((nr,nc,cnt))
+# for i in range(n):
+#     print(visited[i])
+print(visited[-1][-1])
