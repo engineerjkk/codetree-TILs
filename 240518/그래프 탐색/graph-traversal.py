248dd9ed956a12d05fcd76a16ea646ea0aa9e5dd
JuneKoo Kang <engineerjkk@naver.com>
2024-05-18 13:35:04 +0900

    Title: 그래프 탐색, Time: 165ms, Memory: 26MB, Status: Passed - CodeTree

--- 240518/그래프 탐색/graph-traversal.py
+++ 240518/그래프 탐색/graph-traversal.py
@@ -1,9 +1,8 @@
 import sys
 input = sys.stdin.readline
-n,m = map(int,input().split())
-
-visited=[False for _ in range(n+1)]
+n,m = tuple(map(int,input().split()))
 graph=[[] for _ in range(n+1)]
+visited=[False for _ in range(n+1)]
 
 vertex_cnt=0
 def dfs(vertex):
@@ -13,6 +12,7 @@ def dfs(vertex):
             visited[node]=True
             vertex_cnt+=1
             dfs(node)
+
 
 for _ in range(m):
     a,b=tuple(map(int,input().split()))
