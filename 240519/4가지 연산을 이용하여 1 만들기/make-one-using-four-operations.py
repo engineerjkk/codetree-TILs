f80afc4d303f94268bc64f5c5b55a2cfc8bd7184
JuneKoo Kang <engineerjkk@naver.com>
2024-05-19 20:10:00 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 146ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240519/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+from itertools import product
+
+n = int(input())
+cal=[0,1,2]
+
+def plus(value):
+    return value+1
+def minus(value):
+    return value-1
+
+def divide(value):
+    if value%2==0:
+        return value//2
+    elif value%3==0:
+        return value//3
+
+queue=deque()
+queue.append(n)
+
+for i in range(1,n):
+    nCr=product(cal,repeat=i)
+    for x in nCr:
+        value=n
+        for j in x:
+            check=True
+            if j==0:
+                value=plus(value)
+            elif j==1:
+                value=minus(value)
+            else:
+                if value%3!=0 and value%2!=0:
+                    check=False
+                else:
+                    value=divide(value)
+            if check==False:
+                break
+            if value==1:
+                print(i)
+                exit()
