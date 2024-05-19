908f61d8ba8291dc5b8092bcc4edd0a561b335fb
JuneKoo Kang <engineerjkk@naver.com>
2024-05-19 22:41:11 +0900

    Title: 괄호 쌍 만들어주기, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240519/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240519/괄호 쌍 만들어주기/pair-parentheses.py
@@ -4,13 +4,15 @@ input = sys.stdin.readline
 lst=input()
 R=[0]*len(lst)
 n=len(lst)-1
-cnt=1
+cnt=0
 for i in range(n-1,-1,-1):
     if lst[i]+lst[i+1]=='))':
-        R[i]=1
+        R[i]=cnt
+        cnt+=1
+    R[i]=cnt
 ans=0    
 for i in range(n-2):
     if lst[i]+lst[i+1]=='((':
-        ans+=sum(R[i:])
+        ans+=R[i]
 
 print(ans)
