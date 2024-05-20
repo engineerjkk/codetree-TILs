d6988947838533058711fc1961a960c71581ea64
JuneKoo Kang <engineerjkk@naver.com>
2024-05-20 11:12:31 +0900

    Title: 이상한 폭탄, Time: 413ms, Memory: 43MB, Status: Passed - CodeTree

--- 240520/이상한 폭탄/strange-bomb.py
+++ 240520/이상한 폭탄/strange-bomb.py
@@ -1,5 +1,5 @@
 import sys
-from collections import defaultdict, deque
+from collections import deque
 
 input = sys.stdin.readline
 
@@ -7,13 +7,17 @@ n, k = map(int, input().split())
 lst = [int(input()) for _ in range(n)]
 
 # 번호별로 위치를 저장할 딕셔너리
-positions = defaultdict(deque)
+positions = {}
 max_number = -1
 
 for i in range(n):
     number = lst[i]
     
-    # 현재 번호가 K 거리 이내에 있는지 확인
+    # 현재 번호에 대한 deque 초기화
+    if number not in positions:
+        positions[number] = deque()
+    
+    # K 거리 이내에 있는지 확인
     while positions[number] and i - positions[number][0] > k:
         positions[number].popleft()
     
