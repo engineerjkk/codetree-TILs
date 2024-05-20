89a117774255ce3d7623af9219e385194b448458
JuneKoo Kang <engineerjkk@naver.com>
2024-05-20 11:36:42 +0900

    Title: 이상한 폭탄, Time: 195ms, Memory: 32MB, Status: Passed - CodeTree

--- 240520/이상한 폭탄/strange-bomb.py
+++ 240520/이상한 폭탄/strange-bomb.py
@@ -1,20 +1,22 @@
 import sys
 input = sys.stdin.readline
-n, k = map(int,input().split())
+n,k = tuple(map(int,input().split()))
 lst=[]
 for _ in range(n):
     lst.append(int(input()))
 
 R=[0]*n
 latest_index=dict()
+
 for i in range(n-1,-1,-1):
     if lst[i] not in latest_index:
         R[i]=-1
     else:
         R[i]=latest_index[lst[i]]
     latest_index[lst[i]]=i
+
 ans=-1
 for i in range(n):
-    if R[i] !=-1 and R[i]-i<=k:
+    if R[i]!=-1 and R[i]-i<=k:
         ans=max(ans,lst[i])
 print(ans)
