38d8747abea485e23b66561b627a60301063d1e0
JuneKoo Kang <engineerjkk@naver.com>
2024-06-06 19:09:04 +0900

    Title: 왕실의 기사 대결, Time: 162ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240606/왕실의 기사 대결/royal-knight-duel.py
+++ 240606/왕실의 기사 대결/royal-knight-duel.py
@@ -9,29 +9,28 @@ C=[0]*MAX_N
 H=[0]*MAX_N
 W=[0]*MAX_N
 K=[0]*MAX_N
-is_moved=[False]*MAX_N
-initial_K=[0]*MAX_N
 nr=[0]*MAX_N
 nc=[0]*MAX_N
+is_moved=[0]*MAX_N
+initial_K=[0]*MAX_N
 dmg=[0]*MAX_N
 dr=[-1,0,1,0]
 dc=[0,1,0,-1]
-
 def try_movement(idx,d):
     for i in range(1,N+1):
         nr[i]=R[i]
         nc[i]=C[i]
-        dmg[i]=0
         is_moved[i]=False
+        dmg[i]=0
     queue=deque()
     queue.append(idx)
-    is_moved[idx]=True #으아 이거 빼먹음
+    is_moved[idx]=True
     while queue:
         r=queue.popleft()
         nr[r]+=dr[d]
         nc[r]+=dc[d]
 
-        if nr[r]<1 or nc[r]<1 or nr[r]+H[r]-1>L or nc[r]+W[r]-1>L:
+        if nr[r]<1 or nc[r]<1 or nr[r]+H[r]-1>L or nc[r]+C[r]-1>L:
             return False
         
         for i in range(nr[r],nr[r]+H[r]):
@@ -40,9 +39,9 @@ def try_movement(idx,d):
                     dmg[r]+=1
                 if space[i][j]==2:
                     return False
-        
+
         for i in range(1,N+1):
-            if K[i]<=0 or is_moved[i]==True:
+            if is_moved[i] or K[i]<=0:
                 continue
             if R[i]>nr[r]+H[r]-1 or nr[r]>R[i]+H[i]-1:
                 continue
@@ -50,9 +49,8 @@ def try_movement(idx,d):
                 continue
             is_moved[i]=True
             queue.append(i)
-    dmg[idx]=0#여기 틀렸음 ㅠ idx인데 r이라고 했네
+    dmg[idx]=0
     return True
-
 
 def move_piece(idx,d):
     if K[idx]<=0:
@@ -62,6 +60,7 @@ def move_piece(idx,d):
             R[i]=nr[i]
             C[i]=nc[i]
             K[i]-=dmg[i]
+    return
 
 L,N,Q=map(int,input().split())
 for i in range(1,L+1):
@@ -69,11 +68,9 @@ for i in range(1,L+1):
 for i in range(1,N+1):
     R[i],C[i],H[i],W[i],K[i]=map(int,input().split())
     initial_K[i]=K[i]
-
 for _ in range(Q):
     idx,d=map(int,input().split())
     move_piece(idx,d)
-
 ans=[]
 for i in range(1,N+1):
     if K[i]>0:
