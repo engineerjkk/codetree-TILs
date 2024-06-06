489abfe316c0a222bdd135307039b25146e24d6e
JuneKoo Kang <engineerjkk@naver.com>
2024-06-07 00:58:59 +0900

    Title: 메이즈 러너, Time: 174ms, Memory: 25MB, Status: Passed - CodeTree

--- 240607/메이즈 러너/maze-runner.py
+++ 240607/메이즈 러너/maze-runner.py
@@ -14,14 +14,12 @@ ans=0
 sr,sc,square_size=0,0,0
 
 def move_all_traveler():
-    global exits, ans
-
+    global exits,ans
     for i in range(1,m+1):
-        if traveler[i]==exits:
+        if traveler[i][0]==exits[0] and traveler[i][1]==exits[1]:
             continue
         tr,tc=traveler[i]
         er,ec=exits
-
         if tr!=er:
             nr,nc=tr,tc
             if er>nr:
@@ -38,7 +36,6 @@ def move_all_traveler():
                 nc+=1
             else:
                 nc-=1
-
             if not space[nr][nc]:
                 traveler[i]=[nr,nc]
                 ans+=1
@@ -48,20 +45,20 @@ def find_minimum_square():
     global exits,sr,sc,square_size
     er,ec=exits
     for size in range(2,n+1):
-        for r1 in range(1,n-size+2):#이래야 범위가 꽉참 +2
-            for c1 in range(1,n-size+2):
-                r2,c2=r1+size-1,c1+size-1
-                if not (r1<=er<=r2 and c1<=ec<=c2):
+        for r in range(1,n-size+2):
+            for c in range(1,n-size+2):
+                r2,c2=r+size-1,c+size-1
+                if not (r<=er<=r2 and c<=ec<=c2):
                     continue
                 is_traveler_in=False
-                for l in range(1,m+1):
-                    tr,tc=traveler[l]
-                    if r1<=tr<=r2 and c1<=tc<=c2:
+                for i in range(1,m+1):
+                    tr,tc=traveler[i]
+                    if r<=tr<=r2 and c<=tc<=c2:
                         if not(tr==er and tc==ec):
                             is_traveler_in=True
                 if is_traveler_in:
-                    sr=r1
-                    sc=c1
+                    sr=r
+                    sc=c
                     square_size=size
                     return
 def rotate_square():
@@ -86,18 +83,18 @@ def rotate_traveler_and_exit():
             Or,Oc=tr-sr,tc-sc
             rr,rc=Oc,square_size-Or-1
             traveler[i]=[rr+sr,rc+sc]
-
     er,ec=exits
     if sr<=er<sr+square_size and sc<=ec<sc+square_size:
         Or,Oc=er-sr,ec-sc
         rr,rc=Oc,square_size-Or-1
         exits=[rr+sr,rc+sc]
 
+
 for _ in range(k):
     move_all_traveler()
     is_all_escaped=True
     for i in range(1,m+1):
-        if traveler[i]!=exits:
+        if traveler[i][0]!=exits[0] or traveler[i][1]!=exits[1]:
             is_all_escaped=False
     if is_all_escaped:
         break
@@ -105,5 +102,4 @@ for _ in range(k):
     rotate_square()
     rotate_traveler_and_exit()
 print(ans)
-er,ec=exits
-print(er,ec)
+print(exits[0],exits[1])
