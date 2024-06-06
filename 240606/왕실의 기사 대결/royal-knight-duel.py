99eb6250f9972a4caa22832bb1014e1dadb6cc07
JuneKoo Kang <engineerjkk@naver.com>
2024-06-06 17:53:09 +0900

    Title: 왕실의 기사 대결, Time: 199ms, Memory: 25MB, Status: Passed - CodeTree

--- 240606/왕실의 기사 대결/royal-knight-duel.py
+++ 240606/왕실의 기사 대결/royal-knight-duel.py
@@ -1,11 +1,8 @@
 from collections import deque
 MAX_N=31
 MAX_L=41
-dr=[-1,0,1,0]
-dc=[0,1,0,-1]
-
 space=[[0]*MAX_L for _ in range(MAX_L)]
-initial_K=[0]*MAX_L
+initial_K=[0]*MAX_N
 R=[0]*MAX_N
 C=[0]*MAX_N
 H=[0]*MAX_N
@@ -15,15 +12,16 @@ nr=[0]*MAX_N
 nc=[0]*MAX_N
 dmg=[0]*MAX_N
 is_moved=[False]*MAX_N
+dr=[-1,0,1,0]
+dc=[0,1,0,-1]
 
 def try_movement(idx,dir):
-    queue=deque()
-    is_pos=True
     for i in range(1,N+1):
         dmg[i]=0
         is_moved[i]=False
         nr[i]=R[i]
         nc[i]=C[i]
+    queue=deque()
     queue.append(idx)
     is_moved[idx]=True
     while queue:
@@ -32,14 +30,12 @@ def try_movement(idx,dir):
         nc[r]+=dc[dir]
         if nr[r]<1 or nc[r]<1 or nr[r]+H[r]-1>L or nc[r]+W[r]-1>L:
             return False
-
         for i in range(nr[r],nr[r]+H[r]):
             for j in range(nc[r],nc[r]+W[r]):
                 if space[i][j]==1:
                     dmg[r]+=1
                 if space[i][j]==2:
                     return False
-
         for i in range(1,N+1):
             if is_moved[i] or K[i]<=0:
                 continue
@@ -52,10 +48,10 @@ def try_movement(idx,dir):
     dmg[idx]=0
     return True
 
-def move_piece(idx,move_dir):
+def move_piece(idx,dir):
     if K[idx]<=0:
         return
-    if try_movement(idx,move_dir):
+    if try_movement(idx,dir):
         for i in range(1,N+1):
             R[i]=nr[i]
             C[i]=nc[i]
@@ -64,13 +60,14 @@ def move_piece(idx,move_dir):
 L,N,Q=map(int,input().split())
 for i in range(1,L+1):
     space[i][1:]=map(int,input().split())
+
 for i in range(1,N+1):
     R[i],C[i],H[i],W[i],K[i]=map(int,input().split())
     initial_K[i]=K[i]
 
 for _ in range(Q):
-    idx,d=map(int,input().split())
-    move_piece(idx,d)
+    idx,dir=map(int,input().split())
+    move_piece(idx,dir)
 
 ans=[]
 for i in range(1,N+1):
