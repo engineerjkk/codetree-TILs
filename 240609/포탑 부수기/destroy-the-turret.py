b90abecb985ddac02fe62a716e1434cf597effa6
JuneKoo Kang <engineerjkk@naver.com>
2024-06-09 13:43:15 +0900

    Title: 포탑 부수기, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240609/포탑 부수기/destroy-the-turret.py
+++ 240609/포탑 부수기/destroy-the-turret.py
@@ -2,17 +2,17 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 n,m,k = map(int,input().split())
-board=[]
+space=[]
 for _ in range(n):
-    board.append(list(map(int,input().split())))
-recent=[[0]*m for _ in range(n)]
-visit=[[0]*m for _ in range(n)]
+    space.append(list(map(int,input().split())))
 back=[[0]*m for _ in range(n)]
+recent=[[0]*m for _ in range(n)]
 is_active=[[False]*m for _ in range(n)]
+visit=[[False]*m for _ in range(n)]
 dr=[0,1,0,-1]
 dc=[1,0,-1,0]
 dr2=[-1,-1,0,1,1,1,0,-1]
-dc2=[0,1,1,1,0,-1,-1,-1]
+dc2=[0,1,1,1,0,-1,-1,-1,]
 turn=0
 
 class Turrent:
@@ -21,24 +21,22 @@ class Turrent:
         self.c=c
         self.recent=recent
         self.power=power
-
 def init():
     global turn
     turn+=1
     for i in range(n):
         for j in range(m):
-            visit[i][j]=False
             is_active[i][j]=False
-
+            visit[i][j]=False
 def awake():
-    live_turret.sort(key=lambda x:(x.power,-x.recent,-(x.r+x.c),-x.c))
+    live_turret.sort(key=lambda x:(x.power,-(x.recent),-(x.r+x.c),-x.c))
     weak_turret=live_turret[0]
     r=weak_turret.r
     c=weak_turret.c
-    board[r][c]+=m+n
+    space[r][c]+=n+m
     recent[r][c]=turn
+    weak_turret.power=space[r][c]
     weak_turret.recent=recent[r][c]
-    weak_turret.power=board[r][c]
     is_active[r][c]=True
 
 def laser_attack():
@@ -47,8 +45,8 @@ def laser_attack():
     sc=weak_turret.c
     power=weak_turret.power
     strong_turret=live_turret[-1]
-    er=strong_turret.r
-    ec=strong_turret.c
+    er=strong_turret
+    ec=strong_turret
     queue=deque()
     queue.append((sr,sc))
     visit[sr][sc]=True
@@ -61,52 +59,64 @@ def laser_attack():
         for i in range(4):
             nr=(r+dr[i]+n)%n
             nc=(c+dc[i]+m)%m
-            if not visit[nr][nc] and board[nr][nc]>0:
+            if not visit[nr][nc] and space[nr][nc]>0:
                 visit[nr][nc]=True
-                back[nr][nc]=(r,c)
                 queue.append((nr,nc))
+                back[nr][nc]=(r,c)
     if can_attack:
-        board[er][ec]-=power
-        board[er][ec]=max(0,board[er][ec])
+        space[er][ec]-=power
+        space[er][ec]=max(0,space[er][ec])
         is_active[er][ec]=True
         cr,cc=back[er][ec]
         while True:
             if cr==sr and cc==sc:
                 break
-            board[cr][cc]-=power//2
-            board[cr][cc]=max(0,board[cr][cc])
+            space[cr][cc]-=power//2
+            space[cr][cc]=max(0,space[cr][cc])
             is_active[cr][cc]=True
             cr,cc=back[cr][cc]
     return can_attack
 
 def bomb_attack():
     weak_turret=live_turret[0]
-    sr,sc,power=weak_turret.r,weak_turret.c,weak_turret.power
+    sr=weak_turret.r
+    sc=weak_turret.c
+    power=weak_turret.power
     strong_turret=live_turret[-1]
-    er,ec=strong_turret.r,strong_turret.c
-    board[er][ec]-=power
-    board[er][ec]=max(0,board[er][ec])
+    er=strong_turret.r
+    ec=strong_turret.c
     is_active[er][ec]=True
+    space[er][ec]-=power
+    space[er][ec]=max(0,space[er][ec])
     for i in range(8):
         nr=(er+dr2[i]+n)%n
         nc=(ec+dc2[i]+m)%m
         if not (sr==nr and sc==nc):
-            board[nr][nc]-=power//2
-            board[nr][nc]=max(0,board[nr][nc])
+            space[nr][nc]-=power//2
+            space[nr][nc]=max(0,space[nr][nc])
             is_active[nr][nc]=True
 
 def reserve():
     for i in range(n):
         for j in range(m):
-            if not is_active[i][j] and board[i][j]>0:
-                board[i][j]+=1
+            if not is_active[i][j] and space[i][j]>0:
+                space[i][j]+=1
+
+    
+    
+
+
+
+
+    
+    
 
 for _ in range(k):
     live_turret=[]
     for i in range(n):
         for j in range(m):
-            if board[i][j]:
-                new_turret=Turrent(i,j,recent[i][j],board[i][j])
+            if space[i][j]>0:
+                new_turret=Turrent(i,j,recent[i][j],space[i][j])
                 live_turret.append(new_turret)
     if len(live_turret)<=1:
         break
@@ -116,8 +126,9 @@ for _ in range(k):
     if not success:
         bomb_attack()
     reserve()
+
 ans=0
 for i in range(n):
     for j in range(m):
-        ans=max(ans,board[i][j])
+        ans=max(ans,space[i][j])
 print(ans)
