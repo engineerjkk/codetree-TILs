62c05f12bd8cc3df7b872b9edee2e714571e9944
JuneKoo Kang <engineerjkk@naver.com>
2024-06-10 18:27:13 +0900

    Title: 포탑 부수기, Time: 198ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240610/포탑 부수기/destroy-the-turret.py
+++ 240610/포탑 부수기/destroy-the-turret.py
@@ -84,8 +84,8 @@ def bomb_attack():
     space[er][ec]=max(0,space[er][ec])
     is_active[er][ec]=True
     for i in range(8):
-        nr=(r+dr[i]+n)%n
-        nc=(c+dc[i]+m)%m
+        nr=(er+dr2[i]+n)%n
+        nc=(ec+dc2[i]+m)%m
         if not (nr==sr and nc==sc):
             space[nr][nc]-=power//2
             space[nr][nc]=max(0,space[nr][nc])
