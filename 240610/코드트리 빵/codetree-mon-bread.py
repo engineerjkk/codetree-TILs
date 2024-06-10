b667da5fce9b900dd13b0c6069519629718785d1
JuneKoo Kang <engineerjkk@naver.com>
2024-06-10 16:21:55 +0900

    Title: 코드트리 빵, Time: 243ms, Memory: 26MB, Status: Passed - CodeTree

--- 240610/코드트리 빵/codetree-mon-bread.py
+++ 240610/코드트리 빵/codetree-mon-bread.py
@@ -57,8 +57,10 @@ def simulate():
                 min_distance=step[nr][nc]
                 min_r,min_c=nr,nc
         people[i]=(min_r,min_c)
+    for i in range(m):
         if people[i]==gs25[i]:
-            space[min_r][min_c]=-1
+            r,c=people[i]
+            space[r][c]=-1
     if time>m:
         return 
     bfs(gs25[time-1])
