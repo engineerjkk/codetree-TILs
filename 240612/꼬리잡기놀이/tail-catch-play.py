b80214e4db7cc28f39b2aa75998ea409a7240471
JuneKoo Kang <engineerjkk@naver.com>
2024-06-12 01:44:54 +0900

    Title: 꼬리잡기놀이, Time: 229ms, Memory: 26MB, Status: Passed - CodeTree

--- 240612/꼬리잡기놀이/tail-catch-play.py
+++ 240612/꼬리잡기놀이/tail-catch-play.py
@@ -1,77 +1,93 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
-dir = [(1,0),(-1,0),(0,1),(0,-1)]
+dr=[-1,0,1,0]
+dc=[0,1,0,-1]
+N,M,K=map(int,input().split())
+space=[]
+for _ in range(N):
+    space.append(list(map(int,input().split())))
 
-N, M, K = map(int, input().split())
-graph = [list(map(int, input().split())) for _  in range(N)]
-q = []
+teams=[]
+def in_range(r,c):
+    return -1<r<N and -1<c<N
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 1:
-            w = deque([(i,j)])
-            e = deque([(i,j)])  # 점수는 인덱스로 계산하기!
-            while w:
-                x,y = w.popleft()
+        if space[i][j]==1:
+            queue=deque()
+            queue.append((i,j))
+            trace=deque()
+            trace.append((i,j))
+            visit=[[False]*N for _ in range(N)]
+            visit[i][j]=True
+            while queue:
+                r,c=queue.popleft()
                 for k in range(4):
-                    nx, ny = x+dir[k][0], y+dir[k][1]
-
-                    if 0<=nx<N and 0<=ny<N and graph[nx][ny] == 2 and (nx,ny) not in e:
-                        e.append((nx,ny))
-                        w.append((nx,ny))
-            x,y = e[-1][0], e[-1][1]
+                    nr=r+dr[k]
+                    nc=c+dc[k]
+                    if in_range(nr,nc) and not visit[nr][nc] and space[nr][nc]==2:
+                        visit[nr][nc]=True
+                        queue.append((nr,nc))
+                        trace.append((nr,nc))
+            r,c=trace[-1]
             for k in range(4):
-                nx, ny = x+dir[k][0], y+dir[k][1]
-                if 0<=nx<N and 0<=ny<N and graph[nx][ny] == 3:
-                    e.append((nx,ny))
+                nr=r+dr[k]
+                nc=c+dc[k]
+                if in_range(nr,nc) and space[nr][nc]==3:
+                    trace.append((nr,nc))
                     break
-            q.append(e)
-# q에는 순서대로 헤드부터 테일까지의 기차들이 들어있다.
+            teams.append(trace)
 
 def move():
-    for train in q:
-        x,y = train.pop()  # tail
-        graph[x][y] = 4    # 원래 선로로 돌려놓기
-        graph[train[-1][0]][train[-1][1]] = 3 # 새로운 꼬리
-
-        x,y = train[0]
-        graph[x][y] = 2    # 원래 머리 위치는 몸통으로 만들기
+    for team in teams:
+        r,c=team.pop()
+        space[r][c]=4
+        space[team[-1][0]][team[-1][1]]=3
+        
+        r,c=team[0]
+        space[r][c]=2
         for i in range(4):
-            nx, ny = x+dir[i][0], y+dir[i][1]
-            if 0<=nx<N and 0<=ny<N and graph[nx][ny] == 4:
-                graph[nx][ny] = 1
-                train.appendleft((nx,ny))
+            nr=r+dr[i]
+            nc=c+dc[i]
+            if in_range(nr,nc) and space[nr][nc]==4:
+                space[nr][nc]=1
+                team.appendleft((nr,nc))
                 break
-
-def ball(round):  # return은 (-1,-1) 즉 좌표로 해서, 큐에 접근하여 뒤바꿀 기차와 점수 알아내기
-    round %= 4*N  # 이것때문에, 문제에서는 라운드1부터이지만 나는 0부터 4N-1까지 할거다.
-    if round<N:  # 첫번째
-        for j in range(N):
-            if graph[round][j] in (1,2,3): return (round,j)
-    elif round<2*N:
+def ball(idx):
+    idx=idx%(4*N)
+    if idx<N:
         for i in range(N):
-            if graph[N-1-i][round-N] in (1,2,3): return (N-1-i,round-N)
-    elif round<3*N:
-        for j in range(N):
-            if graph[3*N-1-round][N-1-j] in (1,2,3): return (3*N-1-round,N-1-j)
+            if space[idx][i] in (1,2,3):
+                return (idx,i)
+    elif idx<2*N:
+        for i in range(N):
+            if space[N-1-i][idx-N] in (1,2,3):
+                return (N-1-i,idx-N)
+    elif idx<3*N:
+        for i in range(N):
+            if space[3*N-1-idx][N-1-i] in (1,2,3):
+                return (3*N-1-idx,N-1-i)
     else:
         for i in range(N):
-            if graph[i][4*N-1-round] in (1,2,3): return(i,4*N-1-round)
+            if space[i][4*N-1-idx] in (1,2,3):
+                return (i,4*N-1-idx)
     return (-1,-1)
 
-def change(x,y):  # 점수를 리턴함
-    if (x,y) == (-1,-1): return 0
-    # (-1,-1) 아닐때만 작동시킴에 유의!
+def change(r,c):
+    if r==-1 and c==-1:
+        return 0
     for i in range(M):
-        if (x,y) in q[i]:
-            for j in range(len(q[i])):
-                if q[i][j] == (x,y):
-                    graph[q[i][0][0]][q[i][0][1]] = 3
-                    graph[q[i][-1][0]][q[i][-1][1]] = 1
-                    q[i].reverse()
+        if (r,c) in teams[i]:
+            for j in range(len(teams[i])):
+                if teams[i][j]==(r,c):
+                    space[teams[i][0][0]][teams[i][0][1]]=3
+                    space[teams[i][-1][0]][teams[i][-1][1]]=1
+                    teams[i].reverse()
                     return (j+1)**2
 
-cnt = 0
+cnt=0
 for i in range(K):
     move()
-    a,b = ball(i)
-    cnt += change(a,b)
+    a,b=ball(i)
+    cnt+=change(a,b)
 print(cnt)
