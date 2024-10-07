345578ed3768c777d485722475ee78250390a471
JuneKoo Kang <engineerjkk@naver.com>
2024-10-08 01:13:41 +0900

    Title: 메이즈 러너, Time: 200ms, Memory: 25MB, Status: Passed - CodeTree

--- 241008/메이즈 러너/maze-runner.py
+++ 241008/메이즈 러너/maze-runner.py
@@ -6,13 +6,14 @@ for _ in range(N):
     space.append(list(map(int,input().split())))
 next_space=[[0]*N for _ in range(N)]
 traveler=[]
-for i in range(M):
+for _ in range(M):
     r,c=map(int,input().split())
     traveler.append((r-1,c-1))
+
 r,c=map(int,input().split())
 Exit=(r-1,c-1)
-sr,sc,square_size=0,0,0
 ans=0
+sr,sc,square_size=0,0,0
 
 def move_traveler():
     global Exit,ans
@@ -21,6 +22,7 @@ def move_traveler():
             continue
         tr,tc=traveler[i]
         er,ec=Exit
+
         if er!=tr:
             nr,nc=tr,tc
             if er>tr:
@@ -43,13 +45,13 @@ def move_traveler():
                 continue
 
 def find_minimum_square():
-    global sr,sc,square_size
+    global sr,sc,square_size,Exit
     for size in range(2,N+1):
         for start_r in range(N-size+1):
             for start_c in range(N-size+1):
                 end_r,end_c=start_r+size-1,start_c+size-1
                 er,ec=Exit
-                if not (start_r<=er<=end_r and start_c<=ec<=end_c):
+                if not(start_r<=er<=end_r and start_c<=ec<=end_c):
                     continue
                 is_in_traveler=False
                 for i in range(M):
@@ -64,7 +66,7 @@ def find_minimum_square():
                     return 
 
 def rotate_square():
-    global sr,sc,square_size,Exit
+    global sr,sc,square_size
     for r in range(sr,sr+square_size):
         for c in range(sc,sc+square_size):
             if space[r][c]>0:
@@ -103,5 +105,6 @@ for _ in range(K):
     find_minimum_square()
     rotate_square()
     rotate_traveler_and_Exit()
+
 print(ans)
 print(Exit[0]+1,Exit[1]+1)
