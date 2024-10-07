0532765c097b6a3b712e3afa0f0ee637c3555a61
JuneKoo Kang <engineerjkk@naver.com>
2024-10-07 21:09:48 +0900

    Title: 왕실의 기사 대결, Time: 272ms, Memory: 25MB, Status: Passed - CodeTree

--- 241007/왕실의 기사 대결/royal-knight-duel.py
+++ 241007/왕실의 기사 대결/royal-knight-duel.py
@@ -4,31 +4,25 @@ from collections import deque
 MAX_L=41
 MAX_N=31
 space=[[0]*MAX_L for _ in range(MAX_L)]
-R=[0]*MAX_N
-C=[0]*MAX_N
-H=[0]*MAX_N
-W=[0]*MAX_N
-K=[0]*MAX_N
-initial_K=[0]*MAX_N
-nr=[0]*MAX_N
-nc=[0]*MAX_N
-dmg=[0]*MAX_N
-is_moved=[False]*MAX_N
+R=[0]*MAX_L
+C=[0]*MAX_L
+H=[0]*MAX_L
+W=[0]*MAX_L
+K=[0]*MAX_L
+initial_K=[0]*MAX_L
+dmg=[0]*MAX_L
+nr=[0]*MAX_L
+nc=[0]*MAX_L
+is_moved=[False]*MAX_L
 dr=[-1,0,1,0]
 dc=[0,1,0,-1]
 
 L,N,Q=map(int,input().split())
-for i in range(1,L+1):
-    space[i][1:]=map(int,input().split())
-
-for i in range(1,N+1):
-    R[i],C[i],H[i],W[i],K[i]=map(int,input().split())
-    initial_K[i]=K[i]
 
 def try_movement(id,d):
     for i in range(1,N+1):
-        is_moved[i]=False
         dmg[i]=0
+        is_moved[i]=False
         nr[i]=R[i]
         nc[i]=C[i]
     
@@ -39,6 +33,7 @@ def try_movement(id,d):
         r=queue.popleft()
         nr[r]+=dr[d]
         nc[r]+=dc[d]
+        
         if nr[r]<1 or nc[r]<1 or nr[r]+H[r]-1>L or nc[r]+W[r]-1>L:
             return False
         
@@ -60,19 +55,22 @@ def try_movement(id,d):
             queue.append(i)
     dmg[id]=0
     return True
-    
-    
 
 def move_piece(id,d):
     if K[id]<=0:
-        return False
-    
+        return
     if try_movement(id,d):
         for i in range(1,N+1):
             R[i]=nr[i]
             C[i]=nc[i]
             K[i]-=dmg[i]
 
+for i in range(1,L+1):
+    space[i][1:]=map(int,input().split())
+
+for i in range(1,N+1):
+    R[i],C[i],H[i],W[i],K[i]=map(int,input().split())
+    initial_K[i]=K[i]
 
 for _ in range(Q):
     id,d=map(int,input().split())
