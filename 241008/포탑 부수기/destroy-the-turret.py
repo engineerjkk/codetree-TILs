3419d1316f5a63311ee6163ff66d8625085ea4c1
JuneKoo Kang <engineerjkk@naver.com>
2024-10-08 20:56:33 +0900

    Title: 포탑 부수기, Time: 511ms, Memory: 27MB, Status: Passed - CodeTree

--- 241008/포탑 부수기/destroy-the-turret.py
+++ 241008/포탑 부수기/destroy-the-turret.py
@@ -1,48 +1,44 @@
 import sys
 input = sys.stdin.readline
 from collections import deque
-
 n,m,k=map(int,input().split())
 space=[]
 for _ in range(n):
     space.append(list(map(int,input().split())))
 recent=[[0]*m for _ in range(n)]
+is_active=[[False]*m for _ in range(n)]
 back=[[0]*m for _ in range(n)]
-is_active=[[0]*m for _ in range(n)]
 visit=[[False]*m for _ in range(n)]
-turn=0
-dr=[-1,0,1,0]
-dc=[0,1,0,-1]
+dr=[0,1,0,-1]
+dc=[1,0,-1,0]
 dr2=[-1,-1,0,1,1,1,0,-1]
 dc2=[0,1,1,1,0,-1,-1,-1]
-
+turn=0
 class Turrent:
     def __init__(self,r,c,recent,power):
         self.r=r
         self.c=c
         self.recent=recent
         self.power=power
-    
+
 def init():
+    global turn
+    turn+=1
     for i in range(n):
         for j in range(m):
             is_active[i][j]=False
             back[i][j]=0
             visit[i][j]=False
-    return
 
 def awake():
-    global turn
-    turn+=1
-    live_turret.sort(key=lambda x:(x.power,-(x.recent),-(x.r+x.c),-x.c))
+    live_turret.sort(key=lambda x:(x.power,-x.recent,-(x.r+x.c),-x.c))
     weak_turret=live_turret[0]
-    sr,sc,power=weak_turret.r,weak_turret.c,weak_turret.power
-    space[sr][sc]+=n+m
-    recent[sr][sc]=turn
-    weak_turret.power=space[sr][sc]
-    weak_turret.recent=recent[sr][sc]
-    is_active[sr][sc]=True
-    return 
+    r,c=weak_turret.r,weak_turret.c
+    space[r][c]+=n+m
+    recent[r][c]=turn
+    weak_turret.power=space[r][c]
+    weak_turret.recent=recent[r][c]
+    is_active[r][c]=True
 
 def laser_attack():
     weak_turret=live_turret[0]
@@ -51,6 +47,7 @@ def laser_attack():
     er,ec=strong_turret.r,strong_turret.c
     queue=deque()
     queue.append((sr,sc))
+    visit=[[False]*m for _ in range(n)]
     visit[sr][sc]=True
     can_attack=False
     while queue:
@@ -62,8 +59,8 @@ def laser_attack():
             nr=(r+dr[i]+n)%n
             nc=(c+dc[i]+m)%m
             if not visit[nr][nc] and space[nr][nc]>0:
-                queue.append((nr,nc))
                 visit[nr][nc]=True
+                queue.append((nr,nc))
                 back[nr][nc]=(r,c)
     if can_attack:
         space[er][ec]-=power
@@ -75,8 +72,8 @@ def laser_attack():
                 break
             space[cr][cc]-=power//2
             space[cr][cc]=max(0,space[cr][cc])
-            cr,cc=back[cr][cc]
             is_active[cr][cc]=True
+            cr,cc=back[cr][cc]
     return can_attack
 
 def bomb_attack():
@@ -90,17 +87,17 @@ def bomb_attack():
     for i in range(8):
         nr=(er+dr2[i]+n)%n
         nc=(ec+dc2[i]+m)%m
-        if not(sr==nr and sc==nc):
+        if not (sr==nr and sc==nc):
             space[nr][nc]-=power//2
             space[nr][nc]=max(0,space[nr][nc])
             is_active[nr][nc]=True
-    return 
 
 def reserve():
     for i in range(n):
         for j in range(m):
             if space[i][j]>0 and not is_active[i][j]:
                 space[i][j]+=1
+
 
 for _ in range(k):
     live_turret=[]
@@ -116,7 +113,6 @@ for _ in range(k):
     if not success:
         bomb_attack()
     reserve()
-
 ans=0
 for i in range(n):
     for j in range(m):
