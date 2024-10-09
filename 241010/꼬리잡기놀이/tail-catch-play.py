952adb6ff6476a9b2b22d7362a5a3326d7f50faa
JuneKoo Kang <engineerjkk@naver.com>
2024-10-10 00:38:45 +0900

    Title: 꼬리잡기놀이, Time: 292ms, Memory: 26MB, Status: Passed - CodeTree

--- 241010/꼬리잡기놀이/tail-catch-play.py
+++ 241010/꼬리잡기놀이/tail-catch-play.py
@@ -2,27 +2,26 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n,m,K=map(int,input().split())
+N,M,K=map(int,input().split())
 space=[]
-for _ in range(n):
+for _ in range(N):
     space.append(list(map(int,input().split())))
-
 dr=[-1,0,1,0]
 dc=[0,1,0,-1]
 
 def in_range(r,c):
-    return -1<r<n and -1<c<n
+    return -1<r<N and -1<c<N
 
 teams=[]
-for i in range(n):
-    for j in range(n):
+for i in range(N):
+    for j in range(N):
         if space[i][j]==1:
+            visit=[[False]*N for _ in range(N)]
+            visit[i][j]=True
             queue=deque()
             queue.append((i,j))
             trace=deque()
             trace.append((i,j))
-            visit=[[False]*n for _ in range(n)]
-            visit[i][j]=True
             while queue:
                 r,c=queue.popleft()
                 for k in range(4):
@@ -36,7 +35,7 @@ for i in range(n):
             for k in range(4):
                 nr=r+dr[k]
                 nc=c+dc[k]
-                if in_range(nr,nc) and not visit[nr][nc] and space[nr][nc]==3:
+                if in_range(nr,nc) and not visit[nr][nc] and space[nr][nc]==3:# 이걸 왜 4라고 했었지 ㅋㅋ
                     trace.append((nr,nc))
                     break
             teams.append(trace)
@@ -45,49 +44,50 @@ def move():
     for team in teams:
         r,c=team.pop()
         space[r][c]=4
-        space[team[-1][0]][team[-1][1]]=3#
+        space[team[-1][0]][team[-1][1]]=3
         r,c=team[0]
-        space[r][c]=2#
+        space[r][c]=2
         for i in range(4):
-            nr=r+dr[i]#
-            nc=c+dc[i]#
+            nr=r+dr[i]
+            nc=c+dc[i]
             if in_range(nr,nc) and space[nr][nc]==4:
                 space[nr][nc]=1
                 team.appendleft((nr,nc))
                 break
 
 def ball(idx):
-    idx=(idx)%(4*n)#
-
-    if idx<n:
-        for c in range(n):
+    idx=(idx)%(4*N)
+    if idx<N:
+        for c in range(N):
             if space[idx][c] in (1,2,3):
                 return (idx,c)
-    elif idx<n*2:
-        for r in reversed(range(n)):#
-            if space[r][idx-n] in (1,2,3):
-                return (r,idx-n)
-    elif idx<n*3:
-        for c in reversed(range(n)):#
-            if space[3*n-1-idx][c] in (1,2,3):
-                return (3*n-1-idx,c)#
+    elif idx<2*N:
+        for r in reversed(range(N)):
+            if space[r][idx-N] in (1,2,3):
+                return (r,idx-N)
+    elif idx<3*N:
+        for c in reversed(range(N)):
+            if space[3*N-1-idx][c] in (1,2,3):
+                return (3*N-1-idx,c)
     else:
-        for r in range(n):
-            if space[r][4*n-1-idx] in (1,2,3):
-                return (r,4*n-1-idx)#
+        for r in range(N):
+            if space[r][4*N-1-idx] in (1,2,3):
+                return (r,4*N-1-idx)
     return (-1,-1)
 
-def catch(r,c):
-    if (r,c)==(-1,-1):
+def catch(a,b):
+    if (a,b)==(-1,-1):
         return 0
-    for i in range(m):
-        if (r,c) in teams[i]:
+
+    for i in range(M):
+        if (a,b) in teams[i]:
             for j in range(len(teams[i])):
-                if teams[i][j]==(r,c):
+                if teams[i][j]==(a,b):
                     space[teams[i][0][0]][teams[i][0][1]]=3
                     space[teams[i][-1][0]][teams[i][-1][1]]=1
                     teams[i].reverse()
-                    return (j+1)**2                    
+                    return (j+1)**2
+
 
 score=0
 for i in range(K):
