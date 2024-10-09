0731c049b3e56db9050dd627645fb184f497a28c
JuneKoo Kang <engineerjkk@naver.com>
2024-10-09 20:50:23 +0900

    Title: 싸움땅, Time: 229ms, Memory: 26MB, Status: Passed - CodeTree

--- 241009/싸움땅/battle-ground.py
+++ 241009/싸움땅/battle-ground.py
@@ -1,15 +1,18 @@
 import sys
 input = sys.stdin.readline
+
 n,m,k=map(int,input().split())
 space=[]
 for _ in range(n):
     space.append(list(map(int,input().split())))
-
 guns=[[[] for _ in range(n)] for _ in range(n)]
 for i in range(n):
     for j in range(n):
         if space[i][j]>0:
             guns[i][j].append(space[i][j])
+points=[0]*m
+dr=[-1,0,1,0]
+dc=[0,1,0,-1]
 empty=(-1,-1,-1,-1,-1,-1)
 
 class Player:
@@ -26,12 +29,9 @@ class Player:
         self.d=(self.d+2)%4
 
 players=[]
-for i in range(m):
+for id in range(m):
     r,c,d,power=map(int,input().split())
-    players.append(Player(i,r-1,c-1,d,power))
-points=[0]*m
-dr=[-1,0,1,0]
-dc=[0,1,0,-1]
+    players.append(Player(id,r-1,c-1,d,power))#
 
 def in_range(r,c):
     return -1<r<n and -1<c<n
@@ -42,7 +42,7 @@ def find_player(nr,nc):
             return player
     return empty
 
-def move_player(player,nr,nc):
+def move(player,nr,nc):
     guns[nr][nc].append(player.gun)
     guns[nr][nc].sort(reverse=True)
     player.gun=guns[nr][nc][0]
@@ -54,23 +54,23 @@ def move_loser(player):
     guns[player.r][player.c].append(player.gun)
     player.gun=0
     for i in range(4):
-        nd=(player.d+i)%4#
+        nd=(player.d+i)%4
         nr=player.r+dr[nd]
         nc=player.c+dc[nd]
         if in_range(nr,nc) and find_player(nr,nc)==empty:
             player.r,player.c,player.d=nr,nc,nd
-            move_player(player,nr,nc)
+            move(player,nr,nc)
             return 
 
 def fight(p1,p2,nr,nc):
     if (p1.power+p1.gun,p1.power)>(p2.power+p2.gun,p2.power):
-        points[p1.id]+=(p1.power+p1.gun)-(p2.power+p2.gun)#
+        points[p1.id]+=(p1.power+p1.gun)-(p2.power+p2.gun)
         move_loser(p2)
-        move_player(p1,nr,nc)#
+        move(p1,nr,nc)
     else:
-        points[p2.id]+=(p2.power+p2.gun)-(p1.power+p1.gun)#
+        points[p2.id]+=(p2.power+p2.gun)-(p1.power+p1.gun)
         move_loser(p1)
-        move_player(p2,nr,nc)#
+        move(p2,nr,nc)
 
 def simulate():
     for player in players:
@@ -79,9 +79,9 @@ def simulate():
             player.change_dir()
             nr,nc=player.get_next()
         next_player=find_player(nr,nc)
-        player.r,player.c=nr,nc #
+        player.r,player.c=nr,nc
         if next_player==empty:
-            move_player(player,nr,nc)
+            move(player,nr,nc)
         else:
             fight(player,next_player,nr,nc)
 
